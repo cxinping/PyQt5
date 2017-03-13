440d58fb1a44a1ce972b65df2624d835d6561101
wangshuo <xpws2006@163.com>
2017-03-14 01:00:04 +0800

    修改表格中单元格的字体和颜色

---
+++ Chapter03/PyQtTableItemFont.py
@@ -0,0 +1,39 @@
+import sys
+from PyQt5.QtWidgets import (QWidget, QTableWidget, QHBoxLayout, QApplication, QTableWidgetItem )
+from PyQt5.QtGui import QBrush,  QColor ,  QFont 
+
+class Table(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle("QTableWidget demo")
+        self.resize(400,300);
+        conLayout = QHBoxLayout()
+        tableWidget=QTableWidget()
+        tableWidget.setRowCount(4)
+        tableWidget.setColumnCount(3)
+        conLayout.addWidget(tableWidget )
+        
+        tableWidget.setHorizontalHeaderLabels(['姓名','性别','体重(kg)'])  
+          
+        newItem = QTableWidgetItem("张三")  
+        newItem.setFont( QFont( "Times", 12, QFont.Black ) )
+        tableWidget.setItem(0, 0, newItem)  
+                 
+        newItem = QTableWidgetItem("男")  
+        newItem.setFont( QFont( "Times", 12, QFont.Black ) )
+        tableWidget.setItem(0, 1, newItem)  
+          
+        newItem = QTableWidgetItem("160") 
+        newItem.setFont( QFont( "Times", 12, QFont.Black ) )
+        tableWidget.setItem(0, 2, newItem)   
+        
+        self.setLayout(conLayout)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    example = Table()  
+    example.show()   
+    sys.exit(app.exec_())
