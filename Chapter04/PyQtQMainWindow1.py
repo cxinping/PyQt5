46f25ec2aea14d47f655963d7c4bc62badbf0d18
wangshuo <xpws2006@163.com>
2017-03-19 18:44:08 +0800

    优化结构

---
+++ Chapter04/PyQtQMainWindow1.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*- 
+'''
+    【简介】
+	PyQT5中主窗口例子
+  
+    作者：信平
+    QQ： 759949947	
+    Email: xpws2006@163.com   
+  
+'''
+
+import sys
+from PyQt5.QtWidgets  import QMainWindow , QApplication
+from PyQt5.QtGui import QIcon 
+
+class MainWindow(QMainWindow):
+    def __init__(self,parent=None):
+        super(MainWindow,self).__init__(parent)
+        self.resize(400, 200)  
+        self.status = self.statusBar()
+        self.status.showMessage("这是状态栏",5000)
+        self.setWindowTitle("PyQt MainWindow例子")      
+
+if __name__ == "__main__":  
+    app = QApplication(sys.argv)
+    app.setWindowIcon(QIcon("./images/cartoon1.ico"))
+    
+    form = MainWindow()
+    form.show()
+    sys.exit(app.exec_())
