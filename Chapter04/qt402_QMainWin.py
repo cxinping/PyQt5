669522eea209315acb8abb4a7ce062bf6f872e76
wangshuo <xpws2006@163.com>
2017-03-30 23:07:23 +0800

    no message

---
+++ Chapter04/qt402_QMainWin.py
@@ -0,0 +1,18 @@
+import sys
+from PyQt5.QtWidgets import QMainWindow , QApplication
+from PyQt5.QtGui import QIcon 
+
+class MainWindow(QMainWindow):
+	def __init__(self,parent=None):
+		super(MainWindow,self).__init__(parent)
+		self.resize(400, 200) 
+		self.status = self.statusBar()
+		self.status.showMessage("ÕâÊÇ×´Ì¬À¸",5000)
+		self.setWindowTitle("PyQt MainWindowÀý×Ó") 
+
+if __name__ == "__main__": 
+	app = QApplication(sys.argv)
+	app.setWindowIcon(QIcon("./images/cartoon1.ico"))
+	form = MainWindow()
+	form.show()
+	sys.exit(app.exec_())
