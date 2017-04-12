896cf92406d7887344a01af3cbabcb028fbe7ef7
wangshuo <xpws2006@163.com>
2017-04-13 01:49:22 +0800

    no message

--- Chapter04/qt0406_QLabel.py
+++ Chapter04/qt0406_QLabel.py
@@ -13,9 +13,9 @@ class Absolute(QWidget):
         l2=QLabel(self)
         l3=QLabel(self)
         l4=QLabel(self)
-        l1.setText("Hello World123")
-        l4.setText("<A href='www.TutorialsPoint.com'>TutorialsPoint</a>")
-        l2.setText("<a href='#'>welcome to Python GUI Programming</a>")
+        l1.setText("Hello World")
+        l4.setText("<A href='http://www.baidu.com'>Baidu</a>")
+        l2.setText("<a href='#'>欢迎使用Python GUI 应用</a>")
         l1.setAlignment(QtCore.Qt.AlignCenter)
         l3.setAlignment(QtCore.Qt.AlignCenter)
         l4.setAlignment(QtCore.Qt.AlignRight)
@@ -31,18 +31,23 @@ class Absolute(QWidget):
         vbox.addWidget(l4)
         
         l1.setOpenExternalLinks(True)
-        #l4.linkActivated.connect( clicked1 )
-        #l2.linkHovered.connect(hovered)
+        # 打开允许访问超链接,默认是不允许，需要使用 setOpenExternalLinks(True)允许浏览器访问超链接
+        l4.setOpenExternalLinks( False )
+        # 点击文本框绑定槽事件  
+        l4.linkActivated.connect( link_clicked )
+        
+        # 划过文本框绑定槽事件       
+        l2.linkHovered.connect( link_hovered )
         l1.setTextInteractionFlags(QtCore.Qt.TextSelectableByMouse)
 
         self.setLayout(vbox)
         self.setWindowTitle("QLabel Demo")
         
-    def hovered():
-        print( "hovering")
+def link_hovered():
+    print( "hovering label2")
         
-    def clicked1():
-        print( "clicked" )
+def link_clicked():
+    print( "clicked label4" )
   
 if __name__ == "__main__":  
     app = QApplication(sys.argv)  
