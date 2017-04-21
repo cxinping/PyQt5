ded23363b860a1f59c1fbdd1bd39f726cec83608
wangshuo <xpws2006@163.com>
2017-04-21 20:51:47 +0800

    no message

--- Chapter08/py08_styleExam02.py
+++ Chapter08/py08_styleExam02.py
@@ -2,15 +2,17 @@
 
 import sys
 from PyQt5.QtWidgets import QMainWindow , QApplication
+from PyQt5.QtGui import QPalette , QBrush , QPixmap
 
 class MainWindow(QMainWindow):
 	def __init__(self,parent=None):
 		super(MainWindow,self).__init__(parent)
 		self.setWindowTitle("界面背景图片设置") 
 		self.resize(350,  250)  
-		self.setObjectName("MainWindow")
-		self.setStyleSheet("#MainWindow{border-image:url(./images/python.jpg);}")
-       
+		palette	= QPalette()
+		palette.setBrush(QPalette.Background,QBrush(QPixmap("./images/python.jpg")))
+		self.setPalette(palette)  
+        
 if __name__ == "__main__": 
 	app = QApplication(sys.argv)
 	form = MainWindow()
