506ff9baaec4e6c0609556b4a6dc01aa0f5f25c4
wangshuo <xpws2006@163.com>
2017-04-21 21:00:09 +0800

    no message

--- Chapter08/py08_styleExam01.py
+++ Chapter08/py08_styleExam01.py
@@ -1,18 +1,19 @@
 # -*- coding: utf-8 -*-
 
+'''
+    【简介】
+	 界面背景图片设置
+    
+'''
+
 import sys
 from PyQt5.QtWidgets import QMainWindow , QApplication
 
-class MainWindow(QMainWindow):
-	def __init__(self,parent=None):
-		super(MainWindow,self).__init__(parent)
-		self.setWindowTitle("界面背景图片设置") 
-		self.resize(350,  250)  
-		self.setObjectName("MainWindow")
-		self.setStyleSheet("#MainWindow{border-image:url(./images/python.jpg);}")
-       
-if __name__ == "__main__": 
-	app = QApplication(sys.argv)
-	form = MainWindow()
-	form.show()
-	sys.exit(app.exec_())
+app = QApplication(sys.argv)
+win = QMainWindow()
+win.setWindowTitle("界面背景图片设置") 
+win.resize(350,  250)  
+win.setObjectName("MainWindow")
+win.setStyleSheet("#MainWindow{border-image:url(./images/python.jpg);}")
+win.show()
+sys.exit(app.exec_())
--- Chapter08/py08_styleExam02.py
+++ Chapter08/py08_styleExam02.py
@@ -1,20 +1,22 @@
 # -*- coding: utf-8 -*-
 
+'''
+    【简介】
+	 界面背景图片设置
+    
+'''
+
 import sys
 from PyQt5.QtWidgets import QMainWindow , QApplication
 from PyQt5.QtGui import QPalette , QBrush , QPixmap
 
-class MainWindow(QMainWindow):
-	def __init__(self,parent=None):
-		super(MainWindow,self).__init__(parent)
-		self.setWindowTitle("界面背景图片设置") 
-		self.resize(350,  250)  
-		palette	= QPalette()
-		palette.setBrush(QPalette.Background,QBrush(QPixmap("./images/python.jpg")))
-		self.setPalette(palette)  
-        
-if __name__ == "__main__": 
-	app = QApplication(sys.argv)
-	form = MainWindow()
-	form.show()
-	sys.exit(app.exec_())
+
+app = QApplication(sys.argv)
+win = QMainWindow()
+win.setWindowTitle("界面背景图片设置") 
+win.resize(350,  250)  
+palette	= QPalette()
+palette.setBrush(QPalette.Background,QBrush(QPixmap("./images/python.jpg")))
+win.setPalette(palette)  
+win.show()
+sys.exit(app.exec_())
