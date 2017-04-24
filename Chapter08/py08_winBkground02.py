3b3e46ce0b3d13ad5d367aa8e16f4edce22a4e4d
wangshuo <xpws2006@163.com>
2017-04-24 17:43:30 +0800

    当背景图片的宽度和高度小于窗口的宽度和高度时

--- Chapter08/py08_winBkground02.py
+++ Chapter08/py08_winBkground02.py
@@ -14,9 +14,13 @@ from PyQt5.QtGui import QPalette , QBrush , QPixmap
 app = QApplication(sys.argv)
 win = QMainWindow()
 win.setWindowTitle("界面背景图片设置") 
-win.resize(350,  250)  
 palette	= QPalette()
 palette.setBrush(QPalette.Background,QBrush(QPixmap("./images/python.jpg")))
 win.setPalette(palette)  
+#当背景图片的宽度和高度大于窗口的宽度和高度时
+#win.resize(460,  255 )  
+
+#当背景图片的宽度和高度小于窗口的宽度和高度时
+win.resize(800,  600 )  
 win.show()
 sys.exit(app.exec_())
