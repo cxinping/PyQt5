09dc7d93a6ed6e80834ed8cca7d9f02bca12c968
wangshuo <xpws2006@163.com>
2017-04-25 21:42:13 +0800

    不规则窗体的实现

--- Chapter08/py08_paint02.py
+++ Chapter08/py08_paint02.py
@@ -22,8 +22,10 @@ class Winform(QWidget):
          
 	def paintEvent(self,event):
 		painter = QPainter(self)
+        #在指定区域直接绘制窗口背景
 		painter.drawPixmap(0,0,self.pix.width(),self.pix.height(),QPixmap("./images/screen1.jpg"))
-		#painter.drawPixmap(0,0,self.width(),self.height(),QPixmap("./images/screen1.jpg"))
+		#绘制窗口背景，平铺到整个窗口，随着窗口改变而改变
+        #painter.drawPixmap(0,0,self.width(),self.height(),QPixmap("./images/screen1.jpg"))
         
 if __name__ == "__main__":  
 		app = QApplication(sys.argv) 
