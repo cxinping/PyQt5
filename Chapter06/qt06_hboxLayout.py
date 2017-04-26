fb1be88895d9147db5c591a3abed1f65e2f07b78
wangshuo <xpws2006@163.com>
2017-04-26 15:01:52 +0800

    布局管理

--- Chapter06/qt06_hboxLayout.py
+++ Chapter06/qt06_hboxLayout.py
@@ -15,14 +15,16 @@ class Winform(QWidget):
     def __init__(self,parent=None):
         super(Winform,self).__init__(parent)
         self.setWindowTitle("水平布局管理例子") 
-        self.initUi()
+        self.initUI()
 
-    def initUi(self):
+    def initUI(self):
         hlayout = QHBoxLayout()
          # 按照从左到右的顺序进行添加按钮部件。
         hlayout.addWidget( QPushButton(str(1)))
         hlayout.addWidget( QPushButton(str(2)))
         hlayout.addWidget( QPushButton(str(3)))
+        hlayout.addWidget( QPushButton(str(4)))        
+        hlayout.addWidget( QPushButton(str(5)))        
         self.setLayout(hlayout)   
   
 if __name__ == "__main__":  
--- Chapter06/qt06_vboxLayout.py
+++ Chapter06/qt06_vboxLayout.py
@@ -15,14 +15,16 @@ class Winform(QWidget):
     def __init__(self,parent=None):
         super(Winform,self).__init__(parent)
         self.setWindowTitle("垂直布局管理例子") 
-        self.initUi()
+        self.initUI()
 
-    def initUi(self):
+    def initUI(self):
         vlayout = QVBoxLayout()
         # 按照从上到下的顺序进行添加按钮部件。
         vlayout.addWidget( QPushButton(str(1)))
         vlayout.addWidget( QPushButton(str(2)))
         vlayout.addWidget( QPushButton(str(3)))
+        vlayout.addWidget( QPushButton(str(4)))
+        vlayout.addWidget( QPushButton(str(5)))
         self.setLayout(vlayout)   
   
 if __name__ == "__main__":  
