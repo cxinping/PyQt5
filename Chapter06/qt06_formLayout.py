48522b0500af8abc6281671cbf8215654f262ba4
wangshuo <xpws2006@163.com>
2017-04-27 14:06:02 +0800

    垂直布局管理例子

---
+++ Chapter06/qt06_formLayout.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+ 
+"""
+    【简介】
+    垂直布局管理例子
+    
+    
+"""
+
+import sys
+from PyQt5.QtWidgets import QApplication  ,QWidget ,QFormLayout , QPushButton, QLineEdit, QLabel
+
+class Winform(QWidget):
+	def __init__(self,parent=None):
+		super(Winform,self).__init__(parent)
+		self.setWindowTitle("窗体布局管理例子") 
+		self.resize(400, 100)  
+         
+		fromlayout = QFormLayout()
+		labl1 = QLabel("标签1");
+		lineEdit1 = QLineEdit();
+		labl2 = QLabel("标签2");
+		lineEdit2 = QLineEdit();
+		labl3 = QLabel("标签3");
+		lineEdit3 = QLineEdit();
+
+		fromlayout.addRow(labl1, lineEdit1);
+		fromlayout.addRow(labl2, lineEdit2);
+		fromlayout.addRow(labl3, lineEdit3);
+		
+		self.setLayout(fromlayout)   
+  
+if __name__ == "__main__":  
+		app = QApplication(sys.argv) 
+		form = Winform()
+		form.show()
+		sys.exit(app.exec_())
