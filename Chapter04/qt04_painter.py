702068cee38e4812c0bffe350a9876c19e17de07
wangshuo <xpws2006@163.com>
2017-04-28 20:27:58 +0800

    打印

--- Chapter04/qt04_painter.py
+++ Chapter04/qt04_painter.py
@@ -6,7 +6,7 @@
     
     
 """
- 
+from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QImage , QIcon, QPixmap
 from PyQt5.QtWidgets import QApplication  , QMainWindow, QLabel,  QSizePolicy , QAction
 from PyQt5.QtPrintSupport import QPrinter, QPrintDialog
