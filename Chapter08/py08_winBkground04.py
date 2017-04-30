f9cd7ca8455972d3c30562116a04df26e47f4f8f
wangshuo <xpws2006@163.com>
2017-05-01 04:24:56 +0800

    paintEvent设置背景

---
+++ Chapter08/py08_winBkground04.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+ 
+"""
+    【简介】
+    paintEvent设置背景
+    
+    
+"""
+
+import sys
+from PyQt5.QtWidgets import QApplication  ,QWidget 
+from PyQt5.QtGui import  QPixmap,   QPainter 
+
+class Winform(QWidget):
+	def __init__(self,parent=None):
+		super(Winform,self).__init__(parent)
+		self.setWindowTitle("paintEvent设置背景") 
+         
+	def paintEvent(self,event):
+		painter = QPainter(self)
+		pixmap = QPixmap("./images/screen1.jpg")
+        #绘制窗口背景，平铺到整个窗口，随着窗口改变而改变
+		painter.drawPixmap(self.rect(),pixmap);
+		
+        
+if __name__ == "__main__":  
+		app = QApplication(sys.argv) 
+		form = Winform()
+		form.show()
+		sys.exit(app.exec_())
