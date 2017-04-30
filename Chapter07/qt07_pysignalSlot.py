57c72ba3fc3fb306f1bc3d01d9fd2caf1fc4615c
wangshuo <xpws2006@163.com>
2017-04-30 21:13:09 +0800

    内置信号槽示例

--- Chapter07/qt07_pysignalSlot.py
+++ Chapter07/qt07_pysignalSlot.py
@@ -10,27 +10,36 @@
 
 from PyQt5.QtCore import QObject , pyqtSignal
 
+#信号对象
 class QTypeSignal(QObject):
-      sendmsg = pyqtSignal( object)
+    #定义一个信号槽
+	sendmsg = pyqtSignal( object)
       
-      def __init__( self):
-          QObject.__init__( self)
+	def __init__( self):
+		super( QTypeSignal, self).__init__()
+            
+	def run( self):
+        # 发射信号
+		self.sendmsg.emit('Hello Pyqt5')
           
-      def run( self):
-          self.sendmsg.emit('发射信号 ')
-          
-          
+# 槽对象          
 class QTypeSlot(QObject):
-      def __init__( self):
-          QObject.__init__( self)
-          
-      def get(self, msg):
-          print("Qslt get msg => " + msg)
+	def __init__( self):
+		super( QTypeSlot, self).__init__()
+    
+   # 槽对象里的槽函数      
+	def get(self, msg):
+		print("QSlot get msg => " + msg)
 
 
 if __name__ == '__main__':
-    send = QTypeSignal()
-    slot = QTypeSlot()
-    send.sendmsg.connect( slot.get)
-    send.run()
+	send = QTypeSignal()
+	slot = QTypeSlot()
+	print('--- 连接信号槽 ---')    
+	send.sendmsg.connect( slot.get)
+	send.run()
+    
+	print('--- 断开信号槽 ---')        
+	send.sendmsg.disconnect( slot.get )
+	send.run()   
 
