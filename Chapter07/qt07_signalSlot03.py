adadd0437806cd0a5ac78d3a3c4df294f98af450
wangshuo <xpws2006@163.com>
2017-04-30 22:46:43 +0800

    信号槽N对N连接、断开连接示例

--- Chapter07/qt07_signalSlot03.py
+++ Chapter07/qt07_signalSlot03.py
@@ -13,43 +13,43 @@ from PyQt5.QtWidgets import *
 class SinClass(QObject):
 
     # 声明一个无参数的信号
-	sin1 = pyqtSignal()
+	signal1 = pyqtSignal()
 
     # 声明带一个int类型参数的信号
-	sin2 = pyqtSignal(int)
+	signal2 = pyqtSignal(int)
 
 	def __init__(self,parent=None):
 		super(SinClass,self).__init__(parent)
 
 		# 信号sin1连接到sin1Call和sin2Call这两个槽
-		self.sin1.connect(self.sin1Call)
-		self.sin1.connect(self.sin2Call)
+		self.signal1.connect(self.sin1Call)
+		self.signal1.connect(self.sin2Call)
 
 		# 信号sin2连接到信号sin1
-		self.sin2.connect(self.sin1)
+		self.signal2.connect(self.signal1)
 
         # 信号发射
-		self.sin1.emit()
-		self.sin2.emit(1)
+		self.signal1.emit()
+		self.signal2.emit(1)
 
 		# 断开sin1、sin2信号与各槽的连接
-		self.sin1.disconnect(self.sin1Call)
-		self.sin1.disconnect(self.sin2Call)
-		self.sin2.disconnect(self.sin1)
+		self.signal1.disconnect(self.sin1Call)
+		self.signal1.disconnect(self.sin2Call)
+		self.signal2.disconnect(self.signal1)
 
 		# 信号sin1和sin2连接同一个槽sin1Call
-		self.sin1.connect(self.sin1Call)
-		self.sin2.connect(self.sin1Call)
+		self.signal1.connect(self.sin1Call)
+		self.signal2.connect(self.sin1Call)
 
         # 信号再次发射
-		self.sin1.emit()
-		self.sin2.emit(1)
+		self.signal1.emit()
+		self.signal2.emit(1)
 
 	def sin1Call(self):
-		print("sin1 emit")
+		print("signal-1 emit")
 
 	def sin2Call(self):
-		print("sin2 emit")
+		print("signal-2 emit")
   
 if __name__ == '__main__':  
 	sin = SinClass()
