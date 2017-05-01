b8e2284a52dcbc0b042d9208b30c20b4d08d9dd3
wangshuo <xpws2006@163.com>
2017-05-01 17:32:24 +0800

    嵌套布局

--- Chapter06/qt06_nestLayout02.py
+++ Chapter06/qt06_nestLayout02.py
@@ -7,38 +7,48 @@
     
 """
 
-
 from PyQt5.QtWidgets import *
- 
+import sys   
  
 class MyWindow(QWidget):  
 
-    def __init__(self):  
-        super().__init__()
-        self.setWindowTitle('PyQt5布局示例')
-        self.resize(400, 300)
+	def __init__(self):  
+		super().__init__()
+		self.setWindowTitle('嵌套布局示例')
+		self.resize(700, 200)
         
         # 全局部件（注意参数 self），用于"承载"全局布局
-        wwg = QWidget(self)
-        
-        wl = QVBoxLayout(wwg) # 全局布局（注意参数 wwg）
+		wwg = QWidget(self)
+		wl = QHBoxLayout(wwg) # 全局布局（注意参数 wwg）
 
-        vl = QVBoxLayout() # 三个局部布局
-        hl = QHBoxLayout()
-        gl = QGridLayout()
+		hlayout =  QHBoxLayout()
+		vlayout =  QVBoxLayout()
+		glayout = QGridLayout()
+		formlayout =  QFormLayout()
         
-        pass # 这里向局部布局内添加部件
-
-        wl.addLayout(vl) # 加到全局布局
-        wl.addLayout(gl)
-        wl.addLayout(hl)
-       
+		hlayout.addWidget( QPushButton(str(1)) ) # 局部布局添加部件（例如：按钮）
+		hlayout.addWidget( QPushButton(str(2)) )
+		vlayout.addWidget( QPushButton(str(3)) )
+		vlayout.addWidget( QPushButton(str(4)) )
+		glayout.addWidget( QPushButton(str(5)) , 0, 0 )
+		glayout.addWidget( QPushButton(str(6)) , 0, 1 )
+		glayout.addWidget( QPushButton(str(7)) , 1, 0)
+		glayout.addWidget( QPushButton(str(8)) , 1, 1)
+		formlayout.addWidget( QPushButton(str(9))  )
+		formlayout.addWidget( QPushButton(str(10)) )
+		formlayout.addWidget( QPushButton(str(11)) )
+		formlayout.addWidget( QPushButton(str(12)) )
+        
+        # 这里向局部布局内添加部件
+		wl.addLayout(hlayout) # 加到全局布局
+		wl.addLayout(vlayout)
+		wl.addLayout(glayout)
+		wl.addLayout(formlayout)       
 
 if __name__=="__main__":    
-    import sys    
-    
-    app = QApplication(sys.argv)    
-    win = MyWindow()  
-    win.show()  
-    sys.exit(app.exec_())
+ 
+	app = QApplication(sys.argv)    
+	win = MyWindow()  
+	win.show()  
+	sys.exit(app.exec_())
     
