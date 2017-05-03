adf758907e45cdc856f0edd08ad47dc32335fd72
wangshuo <xpws2006@163.com>
2017-05-03 23:24:42 +0800

    部件中的信号槽传递额外参数示例

--- Chapter07/qt07_winSignalSlot04.py
+++ Chapter07/qt07_winSignalSlot04.py
@@ -7,9 +7,7 @@
 
 """
 
-from PyQt5.QtCore import *  
-from PyQt5.QtGui import *  
-from PyQt5.QtWidgets import *
+from PyQt5.QtWidgets import QMainWindow, QPushButton , QWidget , QMessageBox, QApplication, QHBoxLayout
 import sys 
 from functools import partial
 
