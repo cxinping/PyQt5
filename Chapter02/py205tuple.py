2384c7f1cd1baf87635ebbf02e7f460a81623f00
wangshuo <xpws2006@163.com>
2017-05-03 09:47:34 +0800

    python基本语法

---
+++ Chapter02/py205tuple.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+#1
+print('\n#1')
+zlst=('hello','PyQt5','.','com')
+vlst=('Top','Quant','.','vip')
+print('zlst,',zlst)
+print('vlst,',vlst)
+
+#2
+print('\n#2')
+s2=zlst[1:];print('s2,',s2)
+s3=zlst[1:3];print('s3,',s3)
+s4=vlst[:3];print('s4,',s4)
+
+#3
+print('\n#3')
+print('s2+s3,',s2+s3)
+print('s3*2,',s3*2)
+
---
+++ Chapter02/py206dict.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+#1
+print('\n#1')
+zdict={}
+zdict['w1']='hello'
+zdict['w2']='ziwang.com'
+print('zdict,',zdict)
+
+
+#2
+print('\n#2')
+vdict={'url1':'TopQuant.vip'
+       ,'url2':'www.TopQuant.vip'
+       ,'url3':'ziwang.com'}
+print('vdict,',vdict)
+
+
+#3
+print('\n#3')
+s2=zdict['w1'];print('s2,',s2)
+s3=vdict['url2'];print('s3,',s3)
