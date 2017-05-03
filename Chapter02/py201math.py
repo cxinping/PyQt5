d9e15e9a933bce97fe97bbdea55f0cb5a6bafe28
wangshuo <xpws2006@163.com>
2017-05-03 09:26:13 +0800

    python基本语法

---
+++ Chapter02/py201math.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+#1
+print('\n#1')
+x=10
+y=22
+z=35
+print('x,y,z,',x,y,z)
+
+#2
+print('\n#2')
+a=x+y;print('a=x+y,',a)
+b=x-y;print('b=x-y,',b)
+c=z-x*y;print('c=z-x*y,',c)
+
+#3
+print('\n#3')
+a=z/x;print('a=z/x,',a)
+b=z//x;print('b=z//x,',b)
+c=z%x;print('c=z%x,',c)
+
+#4
+print('\n#4')
+a=x**2;print('a=x**2,',a)
+b=x**3;print('b=x**3,',b)
