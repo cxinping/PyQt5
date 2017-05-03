2fb649e11714b451f9e8d4fce149583667184a69
wangshuo <xpws2006@163.com>
2017-05-03 09:55:52 +0800

    python基本语法例子

---
+++ Chapter02/py208ctrl.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+#1
+print('\n1,if')
+x,y,z=10,20,5
+if x>y:
+    print('x>y')
+else:
+   print('x<y') 
+
+#2
+print('\n#2,elif')
+x,y,z=10,20,5
+if x>y:
+    print('x>y')
+elif x>z:
+   print('x>z') 
+
+#3
+print('\n#3,whiel')
+x=3
+while x>0:
+    print(x)
+    x-=1
+
+#4
+print('\n#4,for')
+xlst=['1','b','xxx']
+for x in xlst:
+    print(x)
+
+#5
+print('\n#5,for')
+for x in range(3):
+    print(x)
+
+
+
+
+
+
+
+
+
+
