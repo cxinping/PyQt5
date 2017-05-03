dfae9ea59a7e99d86e79227fbea30cd92fe09ab8
wangshuo <xpws2006@163.com>
2017-05-03 16:13:27 +0800

    信号和槽例子

--- Chapter07/qt07_widgetSignalSlot01.py
+++ Chapter07/qt07_widgetSignalSlot01.py
@@ -18,7 +18,7 @@ widget =  QWidget()
 def showMsg():
      QMessageBox.information(widget, "信息提示框", "ok")
      
-btn =  QPushButton( "点我", widget)
-btn.clicked.connect(  showMsg)
+btn = QPushButton( "测试点击按钮", widget)
+btn.clicked.connect( showMsg)
 widget.show()
 sys.exit(app.exec_())
