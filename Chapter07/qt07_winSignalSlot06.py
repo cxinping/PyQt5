2dd40a7dd5f0745e2a9e4a29bd0de826bb1c5614
wangshuo <xpws2006@163.com>
2017-05-04 14:47:37 +0800

    测试代码修正

--- Chapter07/qt07_winSignalSlot06.py
+++ Chapter07/qt07_winSignalSlot06.py
@@ -12,19 +12,21 @@ from PyQt5.QtCore import Qt
 
 class WinForm(QWidget):
     def __init__(self):
-        super().__init__()
-
+        super().__init__()   
         self.initUI()
 
     def initUI(self):
+        #1 先创建滑块和 LCD 部件
         lcd = QLCDNumber(self)
         slider = QSlider(Qt.Horizontal, self)
         
+        #2 通过QVboxLayout来设置布局
         vBox = QVBoxLayout()
         vBox.addWidget(lcd)
         vBox.addWidget(slider)
 
         self.setLayout(vBox)
+        #3 valueChanged()是Qslider的一个信号函数，只要slider的值发生改变，它就会发射一个信号，然后通过connect连接信号的接收部件，也就是lcd。
         slider.valueChanged.connect(lcd.display)
 
         self.setGeometry(300,300,350,150)
@@ -33,6 +35,5 @@ class WinForm(QWidget):
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     form = WinForm()
-    form.show()
-            
+    form.show()                      
     sys.exit(app.exec_())
