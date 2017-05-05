7921f0c8bc77f2518beb28a5c2c8f633177c90a4
wangshuo <xpws2006@163.com>
2017-05-05 10:37:57 +0800

    PyQt5中 QLineEdit例子

---
+++ Chapter04/qt04_lineEdit.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+'''
+    【简介】
+	PyQt5中 QLineEdit例子
+  
+  
+'''
+
+from PyQt5.QtWidgets import QApplication,  QLineEdit , QWidget ,  QFormLayout
+from PyQt5.QtGui import QIntValidator , QDoubleValidator , QFont
+from PyQt5.QtCore import Qt
+import sys  
+    
+def window():
+	app = QApplication(sys.argv)
+	win = QWidget()
+	e1 = QLineEdit()
+	e1.setValidator(QIntValidator())
+	e1.setMaxLength(4)
+	e1.setAlignment(Qt.AlignRight)
+	e1.setFont( QFont("Arial",20))
+	e2 = QLineEdit()
+	e2.setValidator(QDoubleValidator(0.99,99.99,2))
+	flo = QFormLayout()
+	flo.addRow("integer validator", e1)
+	flo.addRow("Double validator",e2)
+	e3 = QLineEdit()
+	e3.setInputMask('+99_9999_999999')
+	flo.addRow("Input Mask",e3)
+	e4 = QLineEdit()
+	e4.textChanged.connect(textchanged)
+	flo.addRow("Text changed",e4)
+	e5=QLineEdit()
+	e5.setEchoMode(QLineEdit.Password)
+	flo.addRow("Password",e5)
+	e6=QLineEdit("Hello Python")
+	e6.setReadOnly(True)
+	flo.addRow("Read Only",e6)
+	e5.editingFinished.connect(enterPress)
+	win.setLayout(flo)
+	win.setWindowTitle("PyQt")
+	win.show()
+	
+	sys.exit(app.exec_())
+
+def textchanged(text):
+	print( "contents of text box: "+text )
+
+def enterPress():
+	print( "edited" )
+    
+    
+if __name__ == "__main__":     
+    window()
