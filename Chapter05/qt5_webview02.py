9793b7e2ede1f1d7ba5b95e5bfc884a217f42093
wangshuo <xpws2006@163.com>
2017-05-06 23:23:24 +0800

    web页面

---
+++ Chapter05/qt5_webview02.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*- 
+
+'''
+    【简介】
+	QWebView打开网页例子 
+  
+'''
+
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtWebEngineWidgets import *
+import sys
+
+class MainWindow(QMainWindow):
+
+	def __init__(self ):
+		super(QMainWindow, self).__init__()
+		self.setWindowTitle('QWebView打开网页例子')
+		self.setGeometry(5, 30, 1355, 730)
+		self.browser = QWebEngineView()
+        #1 加载外部页面
+        #self.browser.load(QUrl('https://www.sogou.com/'))
+		
+        #2 加载本地页面
+		url = r'E:\quant\PyQt5\Chapter05\index.html'
+		self.browser.load( QUrl( url ));
+		
+		self.setCentralWidget(self.browser)
+
+if __name__ == '__main__':
+	app = QApplication(sys.argv)
+    
+	win = MainWindow()
+	win.show()
+	app.exec_()
---
+++ Chapter05/qt5_webview03.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*- 
+
+'''
+    【简介】
+	QWebView打开网页例子 
+  
+'''
+
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtWebEngineWidgets import *
+import sys
+
+class MainWindow(QMainWindow):
+
+	def __init__(self ):
+		super(QMainWindow, self).__init__()
+		self.setWindowTitle('QWebView打开网页例子')
+		self.setGeometry(5, 30, 1355, 730)
+		self.browser = QWebEngineView()
+        #1 加载html代码
+		self.browser = QWebEngineView()
+		self.browser.setHtml('''
+		<!DOCTYPE html>
+		<html>
+			<head>
+				<meta charset="UTF-8">
+				<title></title>
+			</head>
+			<body>
+				<h2>hello PyQt5</h2>
+				
+			</body>
+		</html>
+
+		'''
+		)
+		
+		self.setCentralWidget(self.browser)
+
+if __name__ == '__main__':
+	app = QApplication(sys.argv)
+    
+	win = MainWindow()
+	win.show()
+	app.exec_()
