5d75aafe98687a05d63365a3a5aad3e009621b0a
wangshuo <xpws2006@163.com>
2017-05-06 23:53:55 +0800

    web页面

--- Chapter05/qt5_webview01.py
+++ Chapter05/qt5_webview01.py
@@ -19,18 +19,13 @@ class MainWindow(QMainWindow):
 		self.setWindowTitle('QWebView打开网页例子')
 		self.setGeometry(5, 30, 1355, 730)
 		self.browser = QWebEngineView()
-        #1 加载外部页面
-        #self.browser.load(QUrl('https://www.sogou.com/'))
-		
-        #2 加载本地页面
-		url = r'E:\quant\PyQt5\Chapter05\index.html'
-		self.browser.load( QUrl( url ));
-		
+        # 加载外部页面
+		self.browser.load(QUrl('https://www.sogou.com/'))
+				
 		self.setCentralWidget(self.browser)
 
 if __name__ == '__main__':
-	app = QApplication(sys.argv)
-    
+	app = QApplication(sys.argv)     
 	win = MainWindow()
 	win.show()
-	app.exec_()
+	sys.exit(app.exec_())
--- Chapter05/qt5_webview02.py
+++ Chapter05/qt5_webview02.py
@@ -33,4 +33,4 @@ if __name__ == '__main__':
     
 	win = MainWindow()
 	win.show()
-	app.exec_()
+	sys.exit(app.exec_())
