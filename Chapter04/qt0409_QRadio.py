0e59bec0667ed7bad3740ffffe9216c87aa97909
wangshuo <xpws2006@163.com>
2017-05-08 01:19:45 +0800

    Radio例子

--- Chapter04/qt0409_QRadio.py
+++ Chapter04/qt0409_QRadio.py
@@ -16,28 +16,29 @@ class Radiodemo(QWidget):
 	def __init__(self, parent=None):
 		super(Radiodemo, self).__init__(parent)
 		layout = QHBoxLayout()
-		self.b1=QRadioButton("Button1")
-		self.b1.setChecked(True)
-		self.b1.toggled.connect(lambda:self.btnstate(self.b1))
-		layout.addWidget(self.b1)
-		self.b2=QRadioButton("Button2")
-		self.b2.toggled.connect(lambda:self.btnstate(self.b2))
-		layout.addWidget(self.b2)
+		self.btn1 = QRadioButton("Button1")
+		self.btn1.setChecked(True)
+		self.btn1.toggled.connect(lambda:self.btnstate(self.btn1))
+		layout.addWidget(self.btn1)
+        
+		self.btn2 = QRadioButton("Button2")
+		self.btn2.toggled.connect(lambda:self.btnstate(self.btn2))
+		layout.addWidget(self.btn2)
 		self.setLayout(layout)
 		self.setWindowTitle("RadioButton demo")
 	
-	def btnstate(self,b):
-		if b.text()=="Button1":
-			if b.isChecked() == True:
-				print( b.text() + " is selected" )
+	def btnstate(self,btn):
+		if btn.text()=="Button1":
+			if btn.isChecked() == True:
+				print( btn.text() + " is selected" )
 			else:
-				print( b.text() + " is deselected" )
+				print( btn.text() + " is deselected" )
 		
-		if b.text()=="Button2":
-			if b.isChecked()== True :
-				print( b.text() + " is selected" )
+		if btn.text()=="Button2":
+			if btn.isChecked()== True :
+				print( btn.text() + " is selected" )
 			else:
-				print( b.text() + " is deselected" )
+				print( btn.text() + " is deselected" )
 
 if __name__ == '__main__':
 	app = QApplication(sys.argv)
