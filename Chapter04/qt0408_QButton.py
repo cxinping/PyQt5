c4d8a7ab567f688ef4126cc92ce7a0d9444ca863
wangshuo <xpws2006@163.com>
2017-05-07 22:01:32 +0800

    QButton例子

--- Chapter04/qt0408_QButton.py
+++ Chapter04/qt0408_QButton.py
@@ -16,12 +16,14 @@ class Form(QDialog):
 	def __init__(self, parent=None):
 		super(Form, self).__init__(parent)
 		layout = QVBoxLayout()
+
 		self.b1=QPushButton("Button1")
 		self.b1.setCheckable(True)
 		self.b1.toggle()
 		self.b1.clicked.connect(lambda:self.whichbtn(self.b1) )
 		self.b1.clicked.connect(self.btnstate)
 		layout.addWidget(self.b1)
+            
 		self.b2 = QPushButton()
 		self.b2.setIcon(QIcon(QPixmap("./images/python.png")))
 		self.b2.clicked.connect(lambda:self.whichbtn(self.b2) )
@@ -31,6 +33,7 @@ class Form(QDialog):
 		self.b3=QPushButton("Disabled")
 		self.b3.setEnabled(False)
 		layout.addWidget(self.b3)
+        
 		self.b4= QPushButton("&Download")
 		self.b4.setDefault(True)
 		self.b4.clicked.connect(lambda:self.whichbtn(self.b4))
