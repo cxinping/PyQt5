67bd752da7b2a20a4296b3d916c7d40a94f917d4
wangshuo <xpws2006@163.com>
2017-05-08 22:53:35 +0800

    QCheckBox例子

--- Chapter04/qt0411_QComboBox.py
+++ Chapter04/qt0411_QComboBox.py
@@ -17,16 +17,20 @@ class ComboxDemo(QWidget):
 		super(ComboxDemo, self).__init__(parent)
 		self.setWindowTitle("combox 例子")        
 		self.resize(300, 90)                       
-		layout = QHBoxLayout()
+		layout = QVBoxLayout()
+		self.lbl = QLabel("" )  
+         
 		self.cb = QComboBox()
 		self.cb.addItem("C")
 		self.cb.addItem("C++")
 		self.cb.addItems(["Java", "C#", "Python"])
 		self.cb.currentIndexChanged.connect(self.selectionchange)
 		layout.addWidget(self.cb)
+		layout.addWidget(self.lbl )
 		self.setLayout(layout)
                                     
 	def selectionchange(self,i):
+		self.lbl.setText( self.cb.currentText() )
 		print( "Items in the list are :" )
 		for count in range(self.cb.count()):
 			print( 'item'+str(count) + '='+ self.cb.itemText(count) )
