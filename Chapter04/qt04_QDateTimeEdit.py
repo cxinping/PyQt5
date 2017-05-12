0e035e3254478dd172ec234cb52b509420daf1e9
wangshuo <xpws2006@163.com>
2017-05-12 15:59:33 +0800

    QDateTimeEdit  例子

--- Chapter04/qt04_QDateTimeEdit.py
+++ Chapter04/qt04_QDateTimeEdit.py
@@ -2,7 +2,7 @@
 
 '''
     【简介】
-	PyQt5中 QDateEdit, QTimeEdit 例子
+	PyQt5中 DateTimeEdit 例子
    
   
 '''
@@ -11,32 +11,30 @@ import sys
 #from PyQt5 import QtCore
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
-from PyQt5.QtCore import QDate,  QTime 
+from PyQt5.QtCore import QDate,   QDateTime
 
-class CalendarExample( QWidget):
+class DateTimeEditDemo(QWidget):
 	def __init__(self):
-		super(CalendarExample, self).__init__()
+		super(DateTimeEditDemo, self).__init__()
 		self.initUI()
 		
 	def initUI(self): 
-		self.setWindowTitle('QDateEdit,QTimeEdit例子')
+		self.setWindowTitle('QDateTimeEdit例子')
 		self.resize(300, 90)   
 
 		vlayout = QVBoxLayout()
-		self.dateEdit = QDateEdit(QDate.currentDate(), self)
+		self.dateEdit = QDateTimeEdit(QDateTime.currentDateTime(), self)
+		self.dateEdit.setDisplayFormat("yyyy-MM-dd HH:mm:ss")
         # 设置最小日期
 		self.dateEdit.setMinimumDate(QDate.currentDate().addDays(-365)) 
         # 设置最大日期
 		self.dateEdit.setMaximumDate(QDate.currentDate().addDays(365)) 
 		self.dateEdit.setCalendarPopup( True)
 		
-		self.timeEdit = QTimeEdit(QTime.currentTime(), self)
-		self.timeEdit.setDisplayFormat("HH:mm:ss")
 		self.btn = QPushButton('获得日期和时间')  
 		self.btn.clicked.connect(self.onButtonClick) 
         
 		vlayout.addWidget( self.dateEdit )
-		vlayout.addWidget( self.timeEdit )
 		vlayout.addWidget( self.btn )
 		self.setLayout(vlayout)   
 		
@@ -54,8 +52,7 @@ class CalendarExample( QWidget):
 		minDateTime = self.dateEdit.minimumDateTime() 
 		#最小时间	
 		minTime = self.dateEdit.minimumTime() 
-		
-		time = self.timeEdit.time()	
+		 
 		print('\n#1 选择日期'  )  	
 		print('dateTime=%s' % str(dateTime) ) 
 		print('maxDate=%s' % str(maxDate) ) 
@@ -65,13 +62,9 @@ class CalendarExample( QWidget):
 		print('minDateTime=%s' % str(minDateTime) ) 
 		print('minTime=%s' % str(minTime) ) 
 		
-		print('\n#2 选择时间'  )           
-		print('hour=%d' % time.hour() )           
-		print('minute=%d' % time.minute() )  
-		print('second=%d' % time.hour() )  
         
 if __name__ == '__main__':
 	app = QApplication(sys.argv)
-	demo = CalendarExample()
+	demo = DateTimeEditDemo()
 	demo.show()
 	sys.exit(app.exec_())
