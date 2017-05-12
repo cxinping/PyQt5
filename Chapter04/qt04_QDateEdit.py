2140b4b939abc5c146e148685b9e3e58d8c87680
wangshuo <xpws2006@163.com>
2017-05-12 15:06:55 +0800

    PyQt5中 QDateEdit, QTimeEdit 例子

--- Chapter04/qt04_QDateEdit.py
+++ Chapter04/qt04_QDateEdit.py
@@ -24,6 +24,10 @@ class CalendarExample( QWidget):
 
 		vlayout = QVBoxLayout()
 		self.dateEdit = QDateEdit(QDate.currentDate(), self)
+        # 设置最小日期
+		self.dateEdit.setMinimumDate(QDate.currentDate().addDays(-365)) 
+        # 设置最大日期
+		self.dateEdit.setMaximumDate(QDate.currentDate().addDays(365)) 
 		self.dateEdit.setCalendarPopup( True)
 		
 		self.timeEdit = QTimeEdit(QTime.currentTime(), self)
@@ -37,9 +41,30 @@ class CalendarExample( QWidget):
 		self.setLayout(vlayout)   
 		
 	def onButtonClick(self ):      
-		dateVal = self.dateEdit.dateTime()
-		time = self.timeEdit.time()		
-		print('dateVal=%s' % dateVal )   
+		dateTime  = self.dateEdit.dateTime()
+		#最大日期
+		maxDate = self.dateEdit.maximumDate() 
+		#最大日期时间
+		maxDateTime = self.dateEdit.maximumDateTime() 
+		#最大时间
+		maxTime = self.dateEdit.maximumTime() 
+		#最小日期
+		minDate = self.dateEdit.minimumDate() 
+		#最小日期时间
+		minDateTime = self.dateEdit.minimumDateTime() 
+		#最小时间	
+		minTime = self.dateEdit.minimumTime() 
+		
+		time = self.timeEdit.time()	
+		print('\n#1 选择日期'  )  	
+		print('dateTime=%s' % str(dateTime) ) 
+		print('maxDate=%s' % str(maxDate) ) 
+		print('maxDateTime=%s' % str(maxDateTime) ) 
+		print('maxTime=%s' % str(maxTime) ) 
+		print('minDate=%s' % str(minDate) ) 
+		print('minDateTime=%s' % str(minDateTime) ) 
+		print('minTime=%s' % str(minTime) ) 
+		
 		print('\n#2 选择时间'  )           
 		print('hour=%d' % time.hour() )           
 		print('minute=%d' % time.minute() )  
