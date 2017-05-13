c06ebe9f7684672e4720321380ce0012d4c94085
wangshuo <xpws2006@163.com>
2017-05-14 02:32:33 +0800

    FileDialog例子

--- Chapter04/qt04_QFileDialog.py
+++ Chapter04/qt04_QFileDialog.py
@@ -16,18 +16,18 @@ class filedialogdemo(QWidget):
 	def __init__(self, parent=None):
 		super(filedialogdemo, self).__init__(parent)
 		layout = QVBoxLayout()
-		self.btn = QPushButton("QFileDialog static method demo")
+		self.btn = QPushButton("加载图片")
 		self.btn.clicked.connect(self.getfile)
 		layout.addWidget(self.btn)
-		self.le = QLabel("Hello")
+		self.le = QLabel("")
 		layout.addWidget(self.le)
-		self.btn1 = QPushButton("QFileDialog object")
+		self.btn1 = QPushButton("加载文本文件")
 		self.btn1.clicked.connect(self.getfiles)
 		layout.addWidget(self.btn1)
 		self.contents = QTextEdit()
 		layout.addWidget(self.contents)
 		self.setLayout(layout)
-		self.setWindowTitle("File Dialog demo")
+		self.setWindowTitle("File Dialog 例子")
 
 	def getfile(self):
 		fname, _  = QFileDialog.getOpenFileName(self, 'Open file', 'c:\\',"Image files (*.jpg *.gif)")
