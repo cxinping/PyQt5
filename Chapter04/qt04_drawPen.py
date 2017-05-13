81a61d11d82bf1ff0cc3db4a59d034abafd101aa
wangshuo <xpws2006@163.com>
2017-05-13 15:39:03 +0800

    画图例子

---
+++ Chapter04/qt04_drawPen.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+ 
+"""
+    【简介】
+    绘图中QPen 的例子
+    
+    
+"""
+
+import sys 
+from PyQt5.QtWidgets import *  
+from PyQt5.QtGui import *
+from PyQt5.QtCore import Qt 
+
+class Example(QWidget):
+	def __init__(self):
+		super().__init__()
+
+		self.initUI()
+
+	def initUI(self):
+
+		self.setGeometry(300, 300, 280, 270)
+		self.setWindowTitle('钢笔样式')        
+		self.show()
+
+	def paintEvent(self, e):
+
+		qp = QPainter()
+		qp.begin(self)
+		self.drawLines(qp)
+		qp.end()
+
+	def drawLines(self, qp):
+		pen = QPen(Qt.black, 2, Qt.SolidLine)
+
+		qp.setPen(pen)
+		qp.drawLine(20, 40, 250, 40)
+
+		pen.setStyle(Qt.DashLine)
+		qp.setPen(pen)
+		qp.drawLine(20, 80, 250, 80)
+
+		pen.setStyle(Qt.DashDotLine)
+		qp.setPen(pen)
+		qp.drawLine(20, 120, 250, 120)
+
+		pen.setStyle(Qt.DotLine)
+		qp.setPen(pen)
+		qp.drawLine(20, 160, 250, 160)
+
+		pen.setStyle(Qt.DashDotDotLine)
+		qp.setPen(pen)
+		qp.drawLine(20, 200, 250, 200)
+
+		pen.setStyle(Qt.CustomDashLine)
+		pen.setDashPattern([1, 4, 5, 4])
+		qp.setPen(pen)
+		qp.drawLine(20, 240, 250, 240)
+
+if __name__ == '__main__':
+	app = QApplication(sys.argv)
+	ex = Example()
+	sys.exit(app.exec_())
--- Chapter04/qt04_drawPoint.py
+++ Chapter04/qt04_drawPoint.py
@@ -32,9 +32,8 @@ class Drawing(QWidget):
 		for i in range(1000):
 			# [-100, 100]两个周期的正弦函数图像
 			x = 100 *(-1+2.0*i/1000)+ size.width()/2.0
-			y = -50*math.sin((x - size.width()/2.0)*math.pi/50) + size.height()/2.0
+			y = -50 * math.sin((x - size.width()/2.0)*math.pi/50) + size.height()/2.0
 			qp.drawPoint(x, y)
-
 
 if __name__ == '__main__':
 	app = QApplication(sys.argv)
