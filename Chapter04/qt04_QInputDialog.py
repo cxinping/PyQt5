445d6a36eb13249e0fb2f317eaa994cb257f883d
wangshuo <xpws2006@163.com>
2017-05-14 01:43:00 +0800

    InputDialog例子

--- Chapter04/qt04_QInputDialog.py
+++ Chapter04/qt04_QInputDialog.py
@@ -16,18 +16,20 @@ class InputdialogDemo(QWidget):
 	def __init__(self, parent=None):
 		super(InputdialogDemo, self).__init__(parent)
 		layout = QFormLayout()
-		self.btn = QPushButton("获得列表里的选项")
-		self.btn.clicked.connect(self.getItem)
-		self.le = QLineEdit()
-		layout.addRow(self.btn,self.le)
-		self.btn1 = QPushButton("获得字符串")
-		self.btn1.clicked.connect(self.gettext)
+		self.btn1 = QPushButton("获得列表里的选项")
+		self.btn1.clicked.connect(self.getItem)
 		self.le1 = QLineEdit()
 		layout.addRow(self.btn1,self.le1)
-		self.btn2 = QPushButton("获得整数")
-		self.btn2.clicked.connect(self.getint)
+
+		self.btn2 = QPushButton("获得字符串")
+		self.btn2.clicked.connect(self.gettext)
 		self.le2 = QLineEdit()
 		layout.addRow(self.btn2,self.le2)
+
+		self.btn3 = QPushButton("获得整数")
+		self.btn3.clicked.connect(self.getint)
+		self.le3 = QLineEdit()
+		layout.addRow(self.btn3,self.le3)
 		self.setLayout(layout)
 		self.setWindowTitle("Input Dialog 例子")
 		
@@ -36,17 +38,17 @@ class InputdialogDemo(QWidget):
 		item, ok = QInputDialog.getItem(self, "select input dialog",
 		"语言列表", items, 0, False)
 		if ok and item:
-			self.le.setText(item)
+			self.le1.setText(item)
 	
 	def gettext(self):	
 		text, ok = QInputDialog.getText(self, 'Text Input Dialog', '输入姓名:')
 		if ok:
-			self.le1.setText(str(text)) 
+			self.le2.setText(str(text)) 
 
 	def getint(self):
 		num,ok=QInputDialog.getInt(self,"integer input dualog","输入数字")
 		if ok:
-			self.le2.setText(str(num))
+			self.le3.setText(str(num))
 					
 if __name__ == '__main__':
 	app = QApplication(sys.argv)
