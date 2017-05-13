721040331726741a84f8ef626b5019c5027c9131
wangshuo <xpws2006@163.com>
2017-05-14 01:07:32 +0800

    InputDialog例子

--- Chapter04/qt0418_QInputDialog.py
+++ Chapter04/qt0418_QInputDialog.py
@@ -12,21 +12,21 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 
-class Inputdialogdemo(QWidget):
+class InputdialogDemo(QWidget):
 	def __init__(self, parent=None):
-		super(Inputdialogdemo, self).__init__(parent)
+		super(InputdialogDemo, self).__init__(parent)
 		layout = QFormLayout()
-		self.btn=QPushButton("Choose from list")
+		self.btn = QPushButton("获得列表里的选项")
 		self.btn.clicked.connect(self.getItem)
-		self.le=QLineEdit()
+		self.le = QLineEdit()
 		layout.addRow(self.btn,self.le)
-		self.btn1=QPushButton("get name")
+		self.btn1 = QPushButton("获得字符串")
 		self.btn1.clicked.connect(self.gettext)
-		self.le1=QLineEdit()
+		self.le1 = QLineEdit()
 		layout.addRow(self.btn1,self.le1)
-		self.btn2=QPushButton("Enter an integer")
+		self.btn2 = QPushButton("获得整数")
 		self.btn2.clicked.connect(self.getint)
-		self.le2=QLineEdit()
+		self.le2 = QLineEdit()
 		layout.addRow(self.btn2,self.le2)
 		self.setLayout(layout)
 		self.setWindowTitle("Input Dialog demo")
@@ -34,22 +34,22 @@ class Inputdialogdemo(QWidget):
 	def getItem(self):
 		items = ("C", "C++", "Java", "Python")
 		item, ok = QInputDialog.getItem(self, "select input dialog",
-		"list of languages", items, 0, False)
+		"语言列表", items, 0, False)
 		if ok and item:
 			self.le.setText(item)
 	
 	def gettext(self):	
-		text, ok = QInputDialog.getText(self, 'Text Input Dialog', 'Enter your name:')
+		text, ok = QInputDialog.getText(self, 'Text Input Dialog', '输入姓名:')
 		if ok:
 			self.le1.setText(str(text)) 
 
 	def getint(self):
-		num,ok=QInputDialog.getInt(self,"integer input dualog","enter a number")
+		num,ok=QInputDialog.getInt(self,"integer input dualog","输入数字")
 		if ok:
 			self.le2.setText(str(num))
 					
 if __name__ == '__main__':
 	app = QApplication(sys.argv)
-	demo = Inputdialogdemo()
+	demo = InputdialogDemo()
 	demo.show()
 	sys.exit(app.exec_())
