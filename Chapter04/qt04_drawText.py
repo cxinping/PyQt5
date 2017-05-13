414d3e77838cfc3d1c7828c17494339af70ed36d
wangshuo <xpws2006@163.com>
2017-05-13 13:32:43 +0800

    画图例子

--- Chapter04/qt04_drawText.py
+++ Chapter04/qt04_drawText.py
@@ -16,26 +16,26 @@ class Winform(QWidget):
 	def __init__(self,parent=None):
 		super(Winform,self).__init__(parent)
 		self.setWindowTitle("在窗体中绘画出文字") 
+		self.resize(300, 200)        
 		self.text = '欢迎学习 PyQt5'
          
 	def paintEvent(self,event):
-		painter = QPainter(self)
-     
+		painter = QPainter(self)        
 		painter.begin(self)
-        #自定义的绘画方法
+        # 自定义的绘画方法
 		self.drawText(event, painter)
 		painter.end()
 
 	def drawText(self, event, qp):
-        #设置笔的颜色
+        # 设置笔的颜色
 		qp.setPen( QColor(168, 34, 3) )
-        #设置字体
+        # 设置字体
 		qp.setFont( QFont('SimSun', 20))
-        #画出文本
+        # 画出文本
 		qp.drawText(event.rect(), Qt.AlignCenter, self.text)
 		
 if __name__ == "__main__":  
-		app = QApplication(sys.argv) 
-		form = Winform()
-		form.show()
-		sys.exit(app.exec_())
+	app = QApplication(sys.argv) 
+	form = Winform()
+	form.show()
+	sys.exit(app.exec_())
