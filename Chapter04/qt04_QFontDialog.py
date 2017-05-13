98deb33157a95a8f3eb00545fe7d101fb22cb2ec
wangshuo <xpws2006@163.com>
2017-05-14 02:22:41 +0800

    QFontDialog例子

--- Chapter04/qt04_QFontDialog.py
+++ Chapter04/qt04_QFontDialog.py
@@ -16,18 +16,18 @@ class FontDialogDemo(QWidget):
 	def __init__(self, parent=None):
 		super(FontDialogDemo, self).__init__(parent)
 		layout = QVBoxLayout()
-		self.btn = QPushButton("choose font")
-		self.btn.clicked.connect(self.getFont)
-		layout.addWidget(self.btn)
-		self.le = QLabel("Hello,测试字体例子")
-		layout.addWidget(self.le)
+		self.fontButton  = QPushButton("choose font")
+		self.fontButton .clicked.connect(self.getFont)
+		layout.addWidget(self.fontButton )
+		self.fontLineEdit  = QLabel("Hello,测试字体例子")
+		layout.addWidget(self.fontLineEdit )
 		self.setLayout(layout)
 		self.setWindowTitle("Font Dialog 例子")
 		
 	def getFont(self):
 		font, ok = QFontDialog.getFont()
 		if ok:
-			self.le.setFont(font)
+			self.fontLineEdit .setFont(font)
 					
 if __name__ == '__main__':
 	app = QApplication(sys.argv)
