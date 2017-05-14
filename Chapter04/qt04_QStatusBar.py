5c763b257b5ab10ab299b4b65b5290d018332aa7
wangshuo <xpws2006@163.com>
2017-05-14 20:58:11 +0800

    QStatusBar 例子

--- Chapter04/qt04_QStatusBar.py
+++ Chapter04/qt04_QStatusBar.py
@@ -12,32 +12,24 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 
-class statusdemo(QMainWindow):
+class StatusDemo(QMainWindow):
 	def __init__(self, parent=None):
-		super(statusdemo, self).__init__(parent)
-		bar=self.menuBar()
-		file=bar.addMenu("File")
+		super(StatusDemo, self).__init__(parent)
+		bar = self.menuBar()
+		file = bar.addMenu("File")
 		file.addAction("show")
-		file.addAction("add")
-		file.addAction("remove")
-		file.triggered[QAction].connect(self.processtrigger)
+		file.triggered[QAction].connect(self.processTrigger)
 		self.setCentralWidget(QTextEdit())
 		self.statusBar= QStatusBar() 
-		self.b=QPushButton("click here")
-		self.setWindowTitle("QStatusBar Example")
+		self.setWindowTitle("QStatusBar 例子")
 		self.setStatusBar(self.statusBar)
 	
-	def processtrigger(self,q):
+	def processTrigger(self,q):
 		if (q.text()=="show"):
 			self.statusBar.showMessage(q.text()+" is clicked",2000)
-		if q.text()=="add":
-			self.statusBar.addWidget(self.b)
-		if q.text()=="remove":
-			self.statusBar.removeWidget(self.b) 
-			self.statusBar.show()
-
+	    
 if __name__ == '__main__':
 	app = QApplication(sys.argv)
-	ex = statusdemo()
-	ex.show()
+	demo = StatusDemo()
+	demo.show()
 	sys.exit(app.exec_())
