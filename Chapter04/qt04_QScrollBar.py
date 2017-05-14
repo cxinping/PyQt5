8421a1a06019d685e407b575f49650d09f941543
wangshuo <xpws2006@163.com>
2017-05-14 18:53:50 +0800

    QScrollBar 例子

--- Chapter04/qt04_QScrollBar.py
+++ Chapter04/qt04_QScrollBar.py
@@ -18,25 +18,24 @@ class Example(QWidget):
 		self.initUI()
 		
 	def initUI(self): 
-		#vbox=QVBoxLayout(self)
 		hbox = QHBoxLayout( )
-		self.l1=QLabel("Drag scrollbar sliders to change color")
+		self.l1 = QLabel("拖动滑动条去改变颜色")
 		self.l1.setFont(QFont("Arial",16))
 		hbox.addWidget(self.l1)
-		self.s1=QScrollBar()
+		self.s1 = QScrollBar()
 		self.s1.setMaximum(255)
 		self.s1.sliderMoved.connect(self.sliderval)
-		self.s2=QScrollBar()
+		self.s2 = QScrollBar()
 		self.s2.setMaximum(255)
 		self.s2.sliderMoved.connect(self.sliderval)
-		self.s3=QScrollBar()
+		self.s3 = QScrollBar()
 		self.s3.setMaximum(255)
 		self.s3.sliderMoved.connect(self.sliderval)
 		hbox.addWidget(self.s1)
 		hbox.addWidget(self.s2)
 		hbox.addWidget(self.s3)
 		self.setGeometry(300, 300, 300, 200)
-		self.setWindowTitle('QScrollBar demo')
+		self.setWindowTitle('QScrollBar 例子')
 		self.setLayout( hbox )
 		
 	def sliderval(self):
@@ -48,6 +47,6 @@ class Example(QWidget):
 
 if __name__ == '__main__':
 	app = QApplication(sys.argv)
-	ex = Example() 	
-	ex.show()
+	demo = Example() 	
+	demo.show()
 	sys.exit(app.exec_())
