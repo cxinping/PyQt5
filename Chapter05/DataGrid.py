abc42ecaf943d8c780474da14439e276134bcf7c
wangshuo <xpws2006@163.com>
2017-05-15 14:57:47 +0800

    分页显示

--- Chapter05/DataGrid.py
+++ Chapter05/DataGrid.py
@@ -1,45 +1,78 @@
 # -*- coding: utf-8 -*- 
 
 import sys
-from PyQt5.QtWidgets import (QWidget, QTableWidget, QHBoxLayout, QApplication, QTableWidgetItem )
-from PyQt5.QtGui import QBrush,  QColor 
+from PyQt5.QtWidgets import (QWidget, QTableWidget, QHBoxLayout, QApplication, QPushButton, QLineEdit ,QLabel , QSplitter ,  QTableView , QHeaderView )
+
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
 from PyQt5.QtSql import QSqlDatabase , QSqlQuery
 
 class DataGrid(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.initUI()
 
-    def initUI(self):
-        self.setWindowTitle("QTableWidget demo")
-        self.resize(400,300);
-        conLayout = QHBoxLayout()
-        tableWidget=QTableWidget()
-        tableWidget.setRowCount(4)
-        tableWidget.setColumnCount(3)
-        conLayout.addWidget(tableWidget )
-        
-        tableWidget.setHorizontalHeaderLabels(['姓名','性别','体重(kg)'])  
-          
-        newItem = QTableWidgetItem("张三")  
-        newItem.setForeground(QBrush(QColor(255, 0, 0)))
-        tableWidget.setItem(0, 0, newItem)  
-                 
-        newItem = QTableWidgetItem("男")  
-        newItem.setForeground(QBrush(QColor(255, 0, 0)))
-        tableWidget.setItem(0, 1, newItem)  
-          
-        newItem = QTableWidgetItem("160") 
-        newItem.setForeground(QBrush(QColor(255, 0, 0))) 
-        tableWidget.setItem(0, 2, newItem)     
-        
-        self.setLayout(conLayout)
+	OnPrevButtonClick = pyqtSignal()
+	OnNextButtonClick = pyqtSignal()
+	OnSwitchPageButtonClick = pyqtSignal()
+
+	def __init__(self):
+		super().__init__()
+		self.setWindowTitle("QTableWidget 例子")
+		self.resize(400,300);		
+		self.queryModel = None
+		self.tableView = None		
+		self.totalPageLabel = None
+		self.currentPageLabel = None			
+		self.switchPageLineEdit = None
+		self.prevButton = None		
+		self.nextButton = None
+		self.switchPageButton = None				
+		self.currentPage = 0
+		self.totalPage = 0		
+		self.totalRecrodCount = 0
+		self.PageRecordCount  = 5			
+	
+		self.initUI()
+
+	def initUI(self):
+		# 操作布局
+		operatorLayout = QHBoxLayout()
+		prevButton = QPushButton("前一页")
+		nextButton = QPushButton("下一页")
+		switchPageButton = QPushButton("Go")
+		switchPageLineEdit = QLineEdit()
+		switchPageLineEdit.setFixedWidth(40)	
+		
+		switchPage =  QLabel("转到第")
+		page = QLabel("页")
+		operatorLayout.addWidget(prevButton)
+		operatorLayout.addWidget(nextButton)
+		operatorLayout.addWidget(switchPage)
+		operatorLayout.addWidget(switchPageLineEdit)
+		operatorLayout.addWidget(page)
+		operatorLayout.addWidget(switchPageButton)
+		operatorLayout.addWidget( QSplitter())
+	
+	    # 状态布局
+		statusLayout =  QHBoxLayout()
+		totalPageLabel =  QLabel()
+		totalPageLabel.setFixedWidth(70)
+		currentPageLabel =  QLabel()
+		currentPageLabel.setFixedWidth(70)
+		statusLayout.addWidget(totalPageLabel)
+		statusLayout.addWidget(currentPageLabel)
+		statusLayout.addWidget( QSplitter())	
+		
+		# 设置表格属性
+		tableView = QTableView()
+		#tableView.horizontalHeader().setResizeMode(QHeaderView.Stretch)
+		#tableView.verticalHeader().setResizeMode(QHeaderView.Stretch)
+		#self.tableView.horizontalHeader().setStretchLastSection(True)
+		#self.tableView.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+
+		
+		self.setLayout(operatorLayout)
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    example = DataGrid()  
-    example.show()   
-    sys.exit(app.exec_())
+	app = QApplication(sys.argv)
+	example = DataGrid()  
+	example.show()   
+	sys.exit(app.exec_())
