8dbb4c4702f9e4599131870360feb352998cc417
wangshuo <xpws2006@163.com>
2017-05-15 13:50:09 +0800

    数据库

--- Chapter05/qt05_db01.py
+++ Chapter05/qt05_db01.py
@@ -17,13 +17,13 @@ def createDB():
 	db =  QSqlDatabase.addDatabase('QSQLITE')
 	db.setDatabaseName('./db/sports.db')
 	if not db.open():
-		QtGui.QMessageBox.critical(None, QtGui.qApp.tr("Cannot open database"),
-		QtGui.qApp.tr("Unable to establish a database connection. \n"
+		QMessageBox.critical(None,  ("Cannot open database"),
+		("Unable to establish a database connection. \n"
 		"This example needs SQLite support. Please read "
 		"the Qt SQL driver documentation for information "
 		"how to build it.\n\n"
 		"Click Cancel to exit."),
-		QtGui.QMessageBox.Cancel)
+		QMessageBox.Cancel)
 		return False
 	
 	query = QSqlQuery()
