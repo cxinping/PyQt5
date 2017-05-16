ab0b838b5fb3fa987d6c360ebb8bded5a891f50b
wangshuo <xpws2006@163.com>
2017-05-17 01:49:04 +0800

    TreeWidget 例子

--- Chapter05/DataGrid.py
+++ Chapter05/DataGrid.py
@@ -4,7 +4,7 @@ import sys
 import re
 from PyQt5.QtWidgets import (QWidget , QHBoxLayout , QVBoxLayout , QApplication, QPushButton, QLineEdit ,QLabel , QSplitter ,  QTableView , QHeaderView , QMessageBox )
 from PyQt5.QtCore import Qt
-from PyQt5.QtSql import QSqlDatabase  , QSqlQueryModel
+from PyQt5.QtSql import QSqlDatabase  , QSqlQueryModel , QSqlQuery
 
 def createConnection():
 	# 添加数据库
@@ -14,10 +14,44 @@ def createConnection():
 	# 判断是否打开
 	if not db.open():			
 		return False
+
+	# 声明数据库查询对象
+	query = QSqlQuery()
+	# 创建表
+	query.exec("create table student(id int primary key, name vchar, sex vchar, age int, deparment vchar)")
+	
+	#添加记录
+	query.exec("insert into student values(1,'张三1','男',20,'计算机')")
+	query.exec("insert into student values(2,'李四1','男',19,'经管')")
+	query.exec("insert into student values(3,'王五1','男',22,'机械')")
+	query.exec("insert into student values(4,'赵六1','男',21,'法律')")
+	query.exec("insert into student values(5,'小明1','男',20,'英语')")
+	query.exec("insert into student values(6,'小李1','女',19,'计算机')")
+	query.exec("insert into student values(7,'小张1','男',20,'机械')")
+	query.exec("insert into student values(8,'小刚1','男',19,'经管')")
+	query.exec("insert into student values(9,'张三2','男',21,'计算机')")
+	query.exec("insert into student values(10,'张三3','女',20,'法律')")
+	query.exec("insert into student values(11,'王五2','男',19,'经管')")
+	query.exec("insert into student values(12,'张三4','男',20,'计算机')")
+	query.exec("insert into student values(13,'小李2','男',20,'机械')")
+	query.exec("insert into student values(14,'李四2','女',19,'经管')")
+	query.exec("insert into student values(15,'赵六3','男',21,'英语')")
+	query.exec("insert into student values(16,'李四2','男',19,'法律')")
+	query.exec("insert into student values(17,'小张2','女',22,'经管')")
+	query.exec("insert into student values(18,'李四3','男',21,'英语')")
+	query.exec("insert into student values(19,'小李3','女',19,'法律')")
+	query.exec("insert into student values(20,'王五3','女',20,'机械')")
+	query.exec("insert into student values(21,'张三4','男',22,'计算机')")
+	query.exec("insert into student values(22,'小李2','男',20,'法律')")
+	query.exec("insert into student values(23,'张三5','男',19,'经管')")
+	query.exec("insert into student values(24,'小张3','女',20,'计算机')")
+	query.exec("insert into student values(25,'李四4','男',22,'英语')")
+	query.exec("insert into student values(26,'赵六2','男',20,'机械')")
+	query.exec("insert into student values(27,'小李3','女',19,'英语')")
+	query.exec("insert into student values(28,'王五4','男',21,'经管')")
 	
 	return True		
-
-		
+           		
 class DataGrid(QWidget):
 
 	def __init__(self):
---
+++ Chapter05/qt05_treewidget01.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+'''
+    【简介】
+	PyQT5中 QTreeWidget 例子
+   
+  
+'''
+
+from PyQt5.QtWidgets import *
+import sys
+
+class TreeWidget(QMainWindow):
+
+	def __init__(self,parent=None):
+		super(TreeWidget,self).__init__(parent)
+		self.setWindowTitle('TreeWidget 例子')
+		self.tree = QTreeWidget()
+		self.tree.setColumnCount(2)
+		self.tree.setHeaderLabels(['Key','Value'])
+		root= QTreeWidgetItem(self.tree)
+		root.setText(0,'root')
+		child1 = QTreeWidgetItem(root)
+		child1.setText(0,'child1')
+		child1.setText(1,'name1')
+		child2 = QTreeWidgetItem(root)
+		child2.setText(0,'child2')
+		child2.setText(1,'name2')
+		child3 = QTreeWidgetItem(root)
+		child3.setText(0,'child3')
+		child4 = QTreeWidgetItem(child3)
+		child4.setText(0,'child4')
+		child4.setText(1,'name4')
+		self.tree.addTopLevelItem(root)
+		self.tree.clicked.connect(self.on_tree_widget_clicked)
+		self.setCentralWidget(self.tree)  
+
+	def on_tree_widget_clicked(self, qmodelindex):
+		item = self.tree.currentItem()
+		print("%s is a '%s' " % (item.text(0), item.text(1)))
+        
+if __name__ == '__main__':
+	app = QApplication(sys.argv)
+	tree = TreeWidget()
+	tree.show()
+	sys.exit(app.exec_())
