bd05cdacb9caf12d9ba86e356e26ddd94ce9c548
wangshuo <xpws2006@163.com>
2017-05-17 15:13:23 +0800

    响应事件

--- Chapter05/qt05_treewidget02.py
+++ Chapter05/qt05_treewidget02.py
@@ -21,24 +21,31 @@ class TreeWidgetDemo(QMainWindow):
 		self.tree.setHeaderLabels(['Key','Value'])
 		root= QTreeWidgetItem(self.tree)
 		root.setText(0,'root')
+		root.setText(1,'0')
+		
 		child1 = QTreeWidgetItem(root)
 		child1.setText(0,'child1')
-		child1.setText(1,'name1')
+		child1.setText(1,'1')
+		
 		child2 = QTreeWidgetItem(root)
 		child2.setText(0,'child2')
-		child2.setText(1,'name2')
+		child2.setText(1,'2')
+		
 		child3 = QTreeWidgetItem(root)
 		child3.setText(0,'child3')
+		child3.setText(1,'3')		
+		
 		child4 = QTreeWidgetItem(child3)
 		child4.setText(0,'child4')
-		child4.setText(1,'name4')
+		child4.setText(1,'4')
 
 		child5 = QTreeWidgetItem(child3)
 		child5.setText(0,'child5')
-		child5.setText(1,'name5')
+		child5.setText(1,'5')
         
 		self.tree.addTopLevelItem(root)
 		self.tree.clicked.connect( self.onTreeClicked )
+        		
 		self.setCentralWidget(self.tree)  
 
 	def onTreeClicked(self, qmodelindex):
