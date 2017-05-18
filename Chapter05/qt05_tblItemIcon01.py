fa46d7e39dfcd0e3de9d6720ad9ae0a1977d9e9f
wangshuo <xpws2006@163.com>
2017-05-19 01:02:56 +0800

    单元格添加图片

---
+++ Chapter05/qt05_tblItemIcon01.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*- 
+
+'''
+    【简介】
+	PyQT5中单元格添加图片例子
+   
+  
+'''
+
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore  import *
+
+class Table( QWidget ):
+         
+	def __init__(self):
+		super().__init__()
+		self.initUI()
+
+	def initUI(self):
+		self.setWindowTitle("QTableWidget 例子")
+		self.resize(500,300);
+		conLayout = QHBoxLayout()
+		self.tableWidget= QTableWidget()
+		self.tableWidget.setRowCount(5)
+		self.tableWidget.setColumnCount(4)
+		conLayout.addWidget(self.tableWidget )
+				 
+		self.tableWidget.setHorizontalHeaderLabels(['姓名','性别','体重', '显示图片'])  
+		self.tableWidget.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+		                                		
+		newItem = QTableWidgetItem("张三")    
+		self.tableWidget.setItem(0, 0, newItem)  
+		  
+		newItem = QTableWidgetItem("男")  
+		self.tableWidget.setItem(0, 1, newItem)  
+		  
+		newItem = QTableWidgetItem("160")  
+		self.tableWidget.setItem(0, 2, newItem)   
+		
+		self.tableWidget.setItem(0, 3,  QTableWidgetItem(QIcon("./images/bao1.png"), "背包") ) 
+		self.setLayout(conLayout)
+
+if __name__ == '__main__':
+	app = QApplication(sys.argv)
+	example = Table()  
+	example.show()   
+	sys.exit(app.exec_())
