cf3da8b2d8b96901a2af8687b26f230f0b13c7c3
wangshuo <xpws2006@163.com>
2017-05-20 02:33:47 +0800

    QTimer例子

---
+++ Chapter05/qt05_timer02.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*- 
+'''
+    【简介】
+    PyQT5中QTimer例子
+ 
+  
+'''
+
+from PyQt5.QtWidgets import QWidget,  QPushButton ,  QApplication ,QListWidget,  QGridLayout 
+from PyQt5.QtCore import QTimer
+import sys 
+import time
+
+class WinForm(QWidget):  
+	
+	def __init__(self,parent=None): 
+		super(WinForm,self).__init__(parent) 
+		self.listFile= QListWidget() 
+		self.startBtn = QPushButton('开始') 
+		self.endBtn = QPushButton('结束') 
+		layout = QGridLayout(self) 
+		
+		#self.timer = QTimer(self) #初始化一个定时器
+		#self.timer.timeout.connect(self.operate) #计时结束调用operate()方法
+		#self.timer.start(2000) #设置计时间隔并启动
+
+		layout.addWidget(self.listFile,0,0,1,2) 
+
+		layout.addWidget(self.startBtn,1,0) 
+		layout.addWidget(self.endBtn,1,1) 		
+		
+		self.startBtn.clicked.connect( self.startTimer) 
+		self.endBtn.clicked.connect( self.endTimer) 
+				
+		self.setLayout(layout)   
+		
+	def slotAdd(self): 
+		for n in range(10): 
+			str_n='File index {0}'.format(n) 
+			self.listFile.addItem(str_n) 
+			QApplication.processEvents() 
+			time.sleep(1) 
+
+	def startTimer(self): 
+		print('--- startTimer ')
+
+	def endTimer(self): 
+		print('--- endTimer ')
+	
+if __name__ == "__main__":  
+	app = QApplication(sys.argv)  
+	form = WinForm()  
+	form.show()  
+	sys.exit(app.exec_())
