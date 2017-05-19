90119dee08fb812687b7579e1abe847a8d15fd17
wangshuo <xpws2006@163.com>
2017-05-19 21:05:00 +0800

    实时刷新界面例子

---
+++ Chapter05/qt05_timer03.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*- 
+'''
+    【简介】
+    PyQT5中实时刷新界面例子
+ 
+  
+'''
+
+from PyQt5.QtWidgets import QWidget,  QPushButton ,  QApplication ,QListWidget,  QGridLayout 
+import sys 
+import time
+
+class WinForm(QWidget):  
+	
+	def __init__(self,parent=None): 
+		super(WinForm,self).__init__(parent) 
+		self.listFile= QListWidget() 
+		self.btnStart = QPushButton('开始') 
+		layout = QGridLayout(self) 
+		layout.addWidget(self.listFile,0,0,1,2) 
+		layout.addWidget(self.btnStart,1,1) 
+		self.btnStart.clicked.connect( self.slotAdd) 
+		self.setLayout(layout)   
+		
+	def slotAdd(self): 
+		for n in range(10): 
+			str_n='File index {0}'.format(n) 
+			self.listFile.addItem(str_n) 
+			QApplication.processEvents() 
+			time.sleep(1) 
+		
+if __name__ == "__main__":  
+	app = QApplication(sys.argv)  
+	form = WinForm()  
+	form.show()  
+	sys.exit(app.exec_())
