6dd6385c48cedf3c3457ea5ae341cfcaecad1999
wangshuo <xpws2006@163.com>
2017-05-20 23:15:44 +0800

    优化代码

--- Chapter03/tool.py
+++ Chapter03/tool.py
@@ -2,16 +2,17 @@
 
 '''
     【简介】
-	转换工具
+	ui转换成py的转换工具
      
 '''
 
 import os 
 import os.path 
 
-  
+# UI文件所在的路径 
 dir = './'  
 
+# 列出目录下的所有ui文件
 def listUiFile(): 
 	list = []
 	files = os.listdir(dir)  
@@ -22,10 +23,12 @@ def listUiFile():
 			list.append(filename)
 	
 	return list
-  
+
+# 把后缀为ui的文件改成后缀为py的文件名	
 def transPyFile(filename): 
 	return os.path.splitext(filename)[0] + '.py' 
 
+#调用系统命令把ui转换成py
 def runMain():
 	list = listUiFile()
 	for uifile in list :
@@ -33,6 +36,7 @@ def runMain():
 		cmd = 'pyuic5 -o {pyfile} {uifile}'.format(pyfile=pyfile,uifile=uifile)  
 		#print(cmd)
 		os.system(cmd)
-	
+
+### 程序的主入口		
 if __name__ == "__main__":  	
 	runMain()
