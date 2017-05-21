0a1c05b1caba214da90b2f50fa6bdef057e2f0f5
wangshuo <xpws2006@163.com>
2017-05-21 20:53:13 +0800

    实现界面与算法的分离

--- Chapter03/CallFirstMainWin.py
+++ Chapter03/CallFirstMainWin.py
@@ -4,11 +4,13 @@ import sys
 from PyQt5.QtWidgets import QApplication , QMainWindow
 from firstMainWin import *
 
-if __name__ == '__main__':
-	
-	app = QApplication(sys.argv)
-	mainWindow = QMainWindow()
-	ui = Ui_MainWindow()
-	ui.setupUi(mainWindow)
-	mainWindow.show()
-	sys.exit(app.exec_())
+class MyMainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):    
+        super(MyMainWindow, self).__init__(parent)
+        self.setupUi(self)
+            
+if __name__=="__main__":  
+    app = QApplication(sys.argv)  
+    myWin = MyMainWindow()  
+    myWin.show()  
+    sys.exit(app.exec_())  
