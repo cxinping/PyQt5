dddd9e77962df9781dbf447189b0f64689e2255d
wangshuo <xpws2006@163.com>
2017-05-22 11:19:00 +0800

    pyqt 国际化

--- Chapter03/zh_CN.ts
+++ Chapter03/zh_CN.ts
@@ -1,16 +1,19 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="2.0">
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
 <context>
     <name>Winform</name>
     <message>
         <location filename="qt03_i18n.py" line="19"/>
         <source>upload</source>
-        <translation type="unfinished"></translation>
+        <translatorcomment>上传文件</translatorcomment>
+        <translation type="unfinished">上传</translation>
     </message>
     <message>
         <location filename="qt03_i18n.py" line="20"/>
         <source>download</source>
-        <translation type="unfinished"></translation>
+        <translatorcomment>下载文件</translatorcomment>
+        <translation type="unfinished">下载</translation>
     </message>
 </context>
 </TS>
