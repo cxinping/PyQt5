c97bd79d706e3e4959156d748503e01d5004bf09
wangshuo <xpws2006@163.com>
2017-05-22 11:31:52 +0800

    pyqt 国际化

--- Chapter03/zh_CN.ts
+++ Chapter03/zh_CN.ts
@@ -6,16 +6,19 @@
     <message>
         <location filename="qt03_i18n01.py" line="18"/>
         <source>upload</source>
+        <translatorcomment>上传</translatorcomment>
         <translation type="unfinished">上传</translation>
     </message>
     <message>
         <location filename="qt03_i18n01.py" line="19"/>
         <source>download</source>
+        <translatorcomment>下载</translatorcomment>
         <translation type="unfinished">下载</translation>
     </message>
     <message>
         <location filename="qt03_i18n01.py" line="16"/>
         <source>title</source>
+        <translatorcomment>国际化例子</translatorcomment>
         <translation type="unfinished">国际化例子</translation>
     </message>
 </context>
