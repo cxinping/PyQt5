5ccbe340824bc93ae54583a715cb9a38f83fbc60
wangshuo <xpws2006@163.com>
2017-05-22 17:26:58 +0800

    测试用例

--- Chapter03/MatrixWinTest.py
+++ Chapter03/MatrixWinTest.py
@@ -12,40 +12,76 @@ import unittest
 from PyQt5.QtWidgets import *
 #from PyQt5.QtGui import *
 from PyQt5.QtTest import QTest
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt , QThread  ,  pyqtSignal
 import CallMatrixWinUi
+import time
 
+#继承 QThread 类
+class BackWorkThread(QThread):  
+	#声明一个信号，同时返回一个str
+	finishSignal = pyqtSignal(str)
+	#构造函数里增加形参
+	def __init__(self, sleepTime,parent=None):
+		super(BackWorkThread, self).__init__(parent)
+		#储存参数
+		self.sleepTime = sleepTime
+
+	#重写 run() 函数，在里面干大事。
+	def run(self):
+		#休眠一段时间
+		time.sleep(self.sleepTime)
+		#休眠结束，发送一个信号告诉主线程窗口
+		self.finishSignal.emit('ok , begin to close Window')
+		
 class MatrixWinTest(unittest.TestCase):
 
     # 初始化工作  
 	def setUp(self):  
-		print('--- setUp ---')
+		print('*** setUp ***')
 		self.app = QApplication(sys.argv)	
 		self.form = CallMatrixWinUi.CallMatrixWinUi()
+		
+		#新建对象，传入参数
+		self.bkThread = BackWorkThread(int(3))
+		#连接子进程的信号和槽函数
+		self.bkThread.finishSignal.connect(self.closeWindow)
+		#开始执行 run() 函数里的内容
+		self.bkThread.start()
+		
 		self.form.show()
-		        	  
+		
+        
 	# 退出清理工作  
 	def tearDown(self):  
-		print('--- tearDown ---')  
-		#self.app.deleteLater()
+		print('*** tearDown ***')  
+	
 		sys.exit(self.app.exec_())
 		
 	def setFormToZero(self):
-		print('* setFormToZero ---')  
+		print('* setFormToZero *')  
 		self.form.ui.tequilaScrollBar.setValue(0)
 
 		# Test the maximum.  This one goes to 11.
 		self.form.ui.tequilaScrollBar.setValue(12)
 		self.assertEqual(self.form.ui.tequilaScrollBar.value(), 11)
 
+	def closeWindow(self):
+		#time.sleep(5)
+		print( '*  closeWindow  *')
+		#qApp = QApplication.instance()
+		#qApp.quit()		
+		self.form.quit()
+		
 	def test_defaults(self):
 		'''Test the GUI in its default state'''
-		print('* test_defaults ---')
+		print('* test_defaults *')
 		self.assertEqual(self.form.ui.tequilaScrollBar.value(), 8 )
 		            		
 		# Push OK with the left mouse button
 		okWidget = self.form.ui.buttonBox.button(self.form.ui.buttonBox.Ok)
 		QTest.mouseClick(okWidget, Qt.LeftButton)
+		
+		
 		
 		
 	# 测试滚动条
@@ -56,6 +92,8 @@ class MatrixWinTest(unittest.TestCase):
         # Push OK with the left mouse button
 		okWidget = self.form.ui.buttonBox.button(self.form.ui.buttonBox.Ok)
 		QTest.mouseClick(okWidget, Qt.LeftButton)
+
+
 
 	
 if __name__ == "__main__":  
