c02c703df1c03ff9152c2c64c3749f9fc08d8a24
wangshuo <xpws2006@163.com>
2017-05-23 15:26:13 +0800

    添加资源文件

---
+++ Chapter03/CallMainWin02.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import sys 	
+from PyQt5.QtWidgets import QApplication , QMainWindow
+from MainWin02 import Ui_Form
+
+class MyMainWindow(QMainWindow, Ui_Form):
+    def __init__(self, parent=None):    
+        super(MyMainWindow, self).__init__(parent)
+        self.setupUi(self)
+            
+if __name__=="__main__":  
+    app = QApplication(sys.argv)  
+    myWin = MyMainWindow()  
+    myWin.show()  
+    sys.exit(app.exec_())  
