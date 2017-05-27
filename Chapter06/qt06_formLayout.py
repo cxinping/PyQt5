c66eca6d6f95d4203b28e570207de1c20efdea93
wangshuo <xpws2006@163.com>
2017-05-27 10:07:49 +0800

    自动化测试测试用例优化

--- Chapter06/qt06_formLayout.py
+++ Chapter06/qt06_formLayout.py
@@ -17,16 +17,16 @@ class Winform(QWidget):
 		self.resize(400, 100)  
          
 		fromlayout = QFormLayout()
-		labl1 = QLabel("标签1");
-		lineEdit1 = QLineEdit();
-		labl2 = QLabel("标签2");
-		lineEdit2 = QLineEdit();
-		labl3 = QLabel("标签3");
-		lineEdit3 = QLineEdit();
+		labl1 = QLabel("标签1")
+		lineEdit1 = QLineEdit()
+		labl2 = QLabel("标签2")
+		lineEdit2 = QLineEdit()
+		labl3 = QLabel("标签3")
+		lineEdit3 = QLineEdit()
 
-		fromlayout.addRow(labl1, lineEdit1);
-		fromlayout.addRow(labl2, lineEdit2);
-		fromlayout.addRow(labl3, lineEdit3);
+		fromlayout.addRow(labl1, lineEdit1)
+		fromlayout.addRow(labl2, lineEdit2)
+		fromlayout.addRow(labl3, lineEdit3)
 		
 		self.setLayout(fromlayout)   
   
