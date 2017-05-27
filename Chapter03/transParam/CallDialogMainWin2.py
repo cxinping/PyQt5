8da300dbf97b4d598695e94f775996bbdae8fa93
wangshuo <xpws2006@163.com>
2017-05-27 11:11:03 +0800

    自动化测试测试用例优化

---
+++ Chapter03/transParam/CallDialogMainWin2.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+
+'''
+    【简介】
+     对话框关闭时返回值给主窗口例子
+    
+  
+'''
+
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from DateDialog2 import DateDialog
+
+class WinForm(QWidget):  
+	
+	def __init__(self, parent=None):  
+		super(WinForm, self).__init__(parent)
+		self.resize(700, 200)
+		self.setWindowTitle('对话框关闭时返回值给主窗口例子') 		
+
+		self.open_btn = QPushButton('OPEN')
+		self.le_btn = QLineEdit(self)
+		self.le1_btn = QLineEdit(self)
+		self.le2_btn = QLineEdit(self)
+		self.le3_btn = QLineEdit(self)
+		self.open_btn.clicked.connect( self.openDialog)
+
+		grid = QGridLayout()
+		grid.addWidget(self.le_btn)
+		grid.addWidget(self.le1_btn)
+		grid.addWidget(self.le2_btn)
+		grid.addWidget(self.le3_btn)
+		grid.addWidget(self.open_btn)
+		self.setLayout(grid)
+
+	def openDialog(self):
+		dialog = DateDialog(self)
+		#self.connect(dialog,SIGNAL('sendDate(QDateTime)'),self,SLOT('receiveDate(QDateTime)'))
+		#self.connect(dialog,SIGNAL('sendDate(QDateTime)'),self,SLOT('copyDate(QDateTime)'))
+		#self.connect(dialog.datetime,SIGNAL('dateTimeChanged(QDateTime)'),self,SLOT('getDate(QDateTime)'))
+		#self.connect(dialog,SIGNAL('Signal_OneParameter_Overload(str)'),self,SLOT('getStrDate(str)'))
+		dialog.Signal_OneParameter.connect(self.getStrDate)
+		
+		dialog.show()
+
+	@pyqtSlot(str)
+	def getStrDate(self,dateStr):
+		self.le3_btn.setText(dateStr)
+		
+	@pyqtSlot(QDateTime)
+	def receiveDate(self,date):
+		self.le_btn.setText(date.toString())
+
+	@pyqtSlot(QDateTime)
+	def getDate(self,date):
+		self.le2_btn.setText(date.toString())
+		
+	@pyqtSlot(QDateTime)
+	def copyDate(self,date):
+		self.le1_btn.setText(date.toString())
+			
+		
+if __name__ == "__main__":  
+	app = QApplication(sys.argv)  
+	form = WinForm()  
+	form.show()  
+	sys.exit(app.exec_())
--- Chapter03/transParam/DateDialog2.py
+++ Chapter03/transParam/DateDialog2.py
@@ -13,35 +13,71 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 
 class DateDialog(QDialog):
-	 Signal_OneParameter = pyqtSignal(str)
+	Signal_OneParameter = pyqtSignal(str)
 	 
 	def __init__(self, parent = None):
 		super(DateDialog, self).__init__(parent)
 
 		layout = QVBoxLayout(self)
 
-		# nice widget for editing the date
 		self.datetime = QDateTimeEdit(self)
 		self.datetime.setCalendarPopup(True)
 		self.datetime.setDateTime(QDateTime.currentDateTime())
+		self.lineedit = QLineEdit(self)
+		self.btn = QPushButton('emit signal')
+		layout.addWidget(self.lineedit)
 		layout.addWidget(self.datetime)
+		layout.addWidget(self.btn)
 
 		# OK and Cancel buttons
 		buttons = QDialogButtonBox(
-		QDialogButtonBox.Ok | QDialogButtonBox.Cancel,
-		Qt.Horizontal, self)
+			QDialogButtonBox.Ok | QDialogButtonBox.Cancel,
+			Qt.Horizontal, self)
 		buttons.accepted.connect(self.accept)
 		buttons.rejected.connect(self.reject)
 		layout.addWidget(buttons)
 
+		self.datetime.dateTimeChanged.connect( lambda: self.showDate(QDateTime) )
+		#self.datetime.dateTimeChanged.connect( lambda: sendDate(QDateTime) )
+		self.btn.clicked.connect( self.myfunc )
+		
+		buttons.accepted.connect( self.clickOkBtn )
+		buttons.rejected.connect( self.clickCancelBtn )		
+
+	def clickOkBtn(self):
+		print('--- clickOkBtn ---')
+		self.Signal_OneParameter.emit('ok')
+		
+	def clickCancelBtn(self):
+		print('--- clickCancelBtn ---')
+		self.Signal_OneParameter.emit('cancel')
+		
+	@pyqtSlot()
+	def myfunc(self):
+		self.Signal_OneParameter.emit((self.datetime.dateTime()).toString())
+		self.lineedit.setText(str((self.datetime.dateTime()).toString()))
+    
+	@pyqtSlot(QDateTime)	
+	def showDate(self,datetime):
+		self.lineedit.setText(datetime.toString())
+		self.update()
+		#print '12345'
+    
 	# get current date and time from the dialog
 	def dateTime(self):
 		return self.datetime.dateTime()
-
-	# static method to create the dialog and return (date, time, accepted)
+		
+    # static method to create the dialog and return (date, time, accepted)
 	@staticmethod
 	def getDateTime(parent = None):
 		dialog = DateDialog(parent)
 		result = dialog.exec_()
 		date = dialog.dateTime()
 		return (date.date(), date.time(), result == QDialog.Accepted)
+		
+		
+		
+		
+		
+		
+		
