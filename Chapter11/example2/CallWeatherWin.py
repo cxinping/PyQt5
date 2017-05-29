9ff1cbc87c813e41e0ab642adab6c91b31d197e7
wangshuo <xpws2006@163.com>
2017-05-29 15:35:47 +0800

    天气查询例子

--- Chapter11/example2/CallWeatherWin.py
+++ Chapter11/example2/CallWeatherWin.py
@@ -12,9 +12,9 @@ class MainWindow(QMainWindow ):
 		self.ui.setupUi(self)
  		
 	def queryWeather(self):
+		print('* queryWeather  ')
 		rep = requests.get('http://www.weather.com.cn/data/sk/101010100.html')
 		rep.encoding = 'utf-8'
-
 		print( rep.json() ) 
 		print('城市: %s' % rep.json()['weatherinfo']['city'] )
 		print('风向: %s' % rep.json()['weatherinfo']['WD'] )
@@ -22,10 +22,10 @@ class MainWindow(QMainWindow ):
 		print('风力: %s' % rep.json()['weatherinfo']['WS'] )
 		print('湿度: %s' % rep.json()['weatherinfo']['SD'] )
 	
-		print('* queryWeather reject ')
+
 
 	def clearResult(self):
-		print('* clearResult reject ')
+		print('* clearResult  ')
 		self.ui.resultText.clear()	
 		
 if __name__=="__main__":  
