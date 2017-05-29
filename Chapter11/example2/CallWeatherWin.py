76369a5029ece7b654fc1a7ad29830ac7a09de9b
wangshuo <xpws2006@163.com>
2017-05-29 15:33:16 +0800

    天气查询例子

--- Chapter11/example2/CallWeatherWin.py
+++ Chapter11/example2/CallWeatherWin.py
@@ -3,6 +3,7 @@
 import sys 	
 from PyQt5.QtWidgets import QApplication , QMainWindow
 from WeatherWin import Ui_Form
+import requests
 
 class MainWindow(QMainWindow ):
 	def __init__(self, parent=None):    
@@ -11,6 +12,16 @@ class MainWindow(QMainWindow ):
 		self.ui.setupUi(self)
  		
 	def queryWeather(self):
+		rep = requests.get('http://www.weather.com.cn/data/sk/101010100.html')
+		rep.encoding = 'utf-8'
+
+		print( rep.json() ) 
+		print('城市: %s' % rep.json()['weatherinfo']['city'] )
+		print('风向: %s' % rep.json()['weatherinfo']['WD'] )
+		print('温度: %s' % rep.json()['weatherinfo']['temp'] + " 度")
+		print('风力: %s' % rep.json()['weatherinfo']['WS'] )
+		print('湿度: %s' % rep.json()['weatherinfo']['SD'] )
+	
 		print('* queryWeather reject ')
 
 	def clearResult(self):
