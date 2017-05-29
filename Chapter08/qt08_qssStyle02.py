540f787d5741892f237bed14accb9e9a9cdc4c98
wangshuo <xpws2006@163.com>
2017-05-29 20:03:11 +0800

    QSS例子

---
+++ Chapter08/qt08_qssStyle02.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+'''
+    【简介】
+	 QSS样式
+    
+'''
+
+from PyQt5.QtWidgets import *
+import sys  
+    
+class WindowDemo(QWidget):  
+	def __init__(self ):  
+		super().__init__()
+		
+		btn1 = QPushButton(self ) 
+		btn1.setProperty( 'flat' , True ) 
+		btn1.setText('按钮1')
+	
+		btn2 = QPushButton(self )  
+		btn2.setProperty( 'flat' , False )
+		btn2.setText('按钮2')	
+		
+		vbox=QVBoxLayout()
+		vbox.addWidget(btn1)
+		vbox.addWidget(btn2)	  
+		self.setLayout(vbox)
+		self.setWindowTitle("QSS样式")
+  
+if __name__ == "__main__":  
+	app = QApplication(sys.argv)  
+	win = WindowDemo()  
+	qssStyle = '''      			  			
+			QPushButton[flat="false"] { 
+				background-color: red 				
+			}
+				
+		'''
+	win.setStyleSheet( qssStyle ) 				
+	win.show()  
+	sys.exit(app.exec_())
+
