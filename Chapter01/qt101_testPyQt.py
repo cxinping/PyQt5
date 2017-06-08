538e0b4d77a662e76b373fff134557f1a37c0146
wangshuo <xpws2006@163.com>
2017-06-08 14:11:21 +0800

    脚本优化

--- Chapter01/qt101_testPyQt.py
+++ Chapter01/qt101_testPyQt.py
@@ -1,6 +1,13 @@
 import sys 	
 from PyQt5 import QtWidgets 
 
+'''
+    【简介】
+	第一个PyQt例子
+   
+    
+'''
+
 app = QtWidgets.QApplication(sys.argv)
 widget = QtWidgets.QWidget()
 widget.resize(360, 360)
