6a7f205b0ff6fbe197a429260a7332313442adc3
wangshuo <xpws2006@163.com>
2017-07-07 16:56:46 +0800

    文件改名

--- Chapter02/py208fun.py
+++ Chapter02/py208fun.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 
 def f01(a,b,c):
-    print('a,b,c,',a,b,c)
-    a2,b2,c2,=a+c,b*2,c*2
-    return a2,b2,c2
+	print('a,b,c,',a,b,c)
+	a2,b2,c2,=a+c,b*2,c*2
+	return a2,b2,c2
     
-
 #1
 print('\n#1')
 x,y,z=f01(1,2,3)
