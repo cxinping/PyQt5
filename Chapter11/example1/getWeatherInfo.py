6f638557aa2a62565e9ea8bafe0acebc3493fa94
wangshuo <xpws2006@163.com>
2017-07-11 13:40:00 +0800

    查询城市天气数据

--- Chapter11/example1/getWeatherInfo.py
+++ Chapter11/example1/getWeatherInfo.py
@@ -10,7 +10,7 @@ import requests
 rep = requests.get('http://www.weather.com.cn/data/sk/101010100.html')
 rep.encoding = 'utf-8'
 
-print( rep.json() ) 
+print('返回结果: %s' % rep.json() ) 
 print('城市: %s' % rep.json()['weatherinfo']['city'] )
 print('风向: %s' % rep.json()['weatherinfo']['WD'] )
 print('温度: %s' % rep.json()['weatherinfo']['temp'] + " 度")
