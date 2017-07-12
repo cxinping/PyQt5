d5f5cd4498e8768f9c79ccd745c5f4da0c7e80ee
wangshuo <xpws2006@163.com>
2017-07-12 23:51:08 +0800

    补充第9章的内容

---
+++ Chapter09/Call_Plotly_PyQt5.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtWebEngineWidgets import *
+from Plotly_PyQt5 import Plotly_PyQt5
+
+
+class MainWindow(QMainWindow ):
+
+	def __init__(self ):
+		super(QMainWindow, self).__init__()
+		#self.setupUi(self)
+		self.plotly_pyqt5 = Plotly_PyQt5()
+		self.qwebengine = QWebEngineView()   		
+		#self.qwebengine.setGeometry(QRect(50, 20, 1200, 600))
+		print( self.plotly_pyqt5.get_plotly_path_if_hs300_bais() )
+		#self.qwebengine.load(QUrl.fromLocalFile(self.plotly_pyqt5.get_plotly_path_if_hs300_bais()))
+
+if __name__ == '__main__':
+	app = QApplication(sys.argv)
+	win = MainWindow()
+	win.showMaximized()
+	app.exec_()
