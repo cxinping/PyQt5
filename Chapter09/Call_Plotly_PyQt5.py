c4845b8a157349d9640e2fb1acf9ec17386546b3
wangshuo <xpws2006@163.com>
2017-07-13 00:19:53 +0800

    Plotly_PyQt5离线加载网页例子

--- Chapter09/Call_Plotly_PyQt5.py
+++ Chapter09/Call_Plotly_PyQt5.py
@@ -12,12 +12,11 @@ class MainWindow(QMainWindow ):
 
 	def __init__(self ):
 		super(QMainWindow, self).__init__()
-		#self.setupUi(self)
 		self.plotly_pyqt5 = Plotly_PyQt5()
 		self.qwebengine = QWebEngineView()   		
-		#self.qwebengine.setGeometry(QRect(50, 20, 1200, 600))
+		self.qwebengine.setGeometry(QRect(50, 20, 1200, 600))
 		print( self.plotly_pyqt5.get_plotly_path_if_hs300_bais() )
-		#self.qwebengine.load(QUrl.fromLocalFile(self.plotly_pyqt5.get_plotly_path_if_hs300_bais()))
+		self.qwebengine.load(QUrl.fromLocalFile(self.plotly_pyqt5.get_plotly_path_if_hs300_bais()))
 
 if __name__ == '__main__':
 	app = QApplication(sys.argv)
