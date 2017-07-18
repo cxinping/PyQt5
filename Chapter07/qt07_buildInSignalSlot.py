88f01c7658e5fb9a9133056ba48bc7982ed6ad3d
wangshuo <xpws2006@163.com>
2017-07-18 21:10:26 +0800

    信号/槽 都是内置的

---
+++ Chapter07/qt07_buildInSignalSlot.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+"""
+    【简介】
+    内置的信号/槽示例
+
+
+"""
+
+#from PyQt5.QtCore import QObject , pyqtSignal
+from PyQt5.QtWidgets import *
+import sys
+
+class Winform(QWidget):
+	def __init__(self,parent=None):
+		super().__init__(parent)
+		self.setWindowTitle('内置的信号/槽示例')
+		self.resize(330,  50 ) 
+		btn = QPushButton('关闭', self)
+		btn.clicked.connect(self.close) 
+         
+if __name__ == '__main__':
+	app = QApplication(sys.argv)
+	win = Winform()
+	win.show()
+	sys.exit(app.exec_())
