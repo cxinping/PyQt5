9155e26ace22711d0499a2133f8c8fb6f1b4e74e
wangshuo <xpws2006@163.com>
2017-07-19 17:35:01 +0800

    完善测试代码

--- Chapter07/qt07_buildInSignalSlot03.py
+++ Chapter07/qt07_buildInSignalSlot03.py
@@ -2,7 +2,7 @@
 
 """
     【简介】
-    自定义信号和槽函数 示例
+    自定义信号和内置槽函数 示例
 
 
 """
@@ -17,21 +17,18 @@ class Winform(QWidget):
 
 	def __init__(self,parent=None):
 		super().__init__(parent)
-		self.setWindowTitle('自定义信号和槽函数示例')
+		self.setWindowTitle('自定义信号和内置槽函数示例')
 		self.resize(330,  50 ) 
 		btn = QPushButton('关闭', self)
 		# 连接 信号和槽
 		btn.clicked.connect(self.btn_clicked)
-		# 接收信号，连接到自定义槽函数
-		self.button_clicked_signal.connect(self.btn_close) 
+		# 接收信号，连接到槽
+		self.button_clicked_signal.connect(self.close) 
 
 	def btn_clicked(self):
 		# 发送自定义信号，无参数
 		self.button_clicked_signal.emit()
-
-	def btn_close(self):
-		self.close()
-		
+		                    		
 if __name__ == '__main__':
 	app = QApplication(sys.argv)
 	win = Winform()
