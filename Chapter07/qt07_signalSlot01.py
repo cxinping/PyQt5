08a84d5f520ce4041b1782eaf6af3a52244c1e4e
wangshuo <xpws2006@163.com>
2017-07-19 10:14:57 +0800

    信号/槽 都是内置的

--- Chapter07/qt07_signalSlot01.py
+++ Chapter07/qt07_signalSlot01.py
@@ -2,7 +2,7 @@
 
 """
     【简介】
-    内置信号槽示例
+    内置信号、自定义槽的演示程序
 
 
 """
@@ -13,14 +13,14 @@ from PyQt5.QtWidgets import QWidget,QPushButton, QApplication
 class Winform(QWidget):
 	def __init__(self):
 		super(Winform, self).__init__()        
-		self.setGeometry(200,300,350,120)
-		self.setWindowTitle("内置信号槽演示程序")
+		self.setGeometry(200,300,350,50)
+		self.setWindowTitle("内置信号、自定义槽的例子")
 		
 		self.btn = QPushButton("按钮文本",self)  
 		self.btn.clicked.connect(self.changeBtnText)  
 	
 	def changeBtnText(self):  
-		self.btn.setText("按钮文本改变了")  
+		self.btn.setText("按钮内容和宽度改变了")  
 		self.btn.setStyleSheet("QPushButton{max-width:200px; min-width:200px}")
 	
 if __name__ == '__main__':
