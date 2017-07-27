4dcf8e660a60f48bbcb3fcbf3cc428ecb9948ce8
wangshuo <xpws2006@163.com>
2017-07-27 20:15:48 +0800

    完善测试代码

--- Chapter01/qt102_PrintApi.py
+++ Chapter01/qt102_PrintApi.py
@@ -11,8 +11,10 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5.QtWebEngineWidgets import QWebEnginePage 
+from PyQt5.QtWidgets import QWidget
+
 out = sys.stdout
-sys.stdout = open(r'E:\QTreeWidgetItemIterator.txt' , 'w')
-help( QTreeWidgetItemIterator  )
+sys.stdout = open(r'E:\QWidget.txt' , 'w')
+help( QWidget  )
 sys.stdout.close()
 sys.stdout = out
