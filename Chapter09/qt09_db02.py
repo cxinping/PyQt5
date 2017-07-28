3ff80cfcaeac1d60e2fadc1b675ad5d811ddfc88
wangshuo <xpws2006@163.com>
2017-07-28 10:35:31 +0800

    重载CloseEvent()事件

--- Chapter09/qt09_db02.py
+++ Chapter09/qt09_db02.py
@@ -11,57 +11,26 @@ import sys
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
-from PyQt5.QtSql import QSqlDatabase , QSqlTableModel
-from PyQt5.QtCore import Qt 
+from PyQt5.QtSql import QSqlDatabase  
 
-def initializeModel(model):
-	model.setTable('people')
-	model.setEditStrategy( QSqlTableModel.OnFieldChange)
-	model.select()
-	model.setHeaderData(0, Qt.Horizontal, "ID")
-	model.setHeaderData(1, Qt.Horizontal, "name")
-	model.setHeaderData(2, Qt.Horizontal, "address")
-	
-def createView(title, model):
-	view =  QTableView()
-	view.setModel(model)
-	view.setWindowTitle(title)
-	return view
-	
-def addrow():
-	ret = model.insertRows( model.rowCount(), 1 )
-	print( 'insertRows=%s' %str(ret) )
+class ExecDatabaseDemo(QWidget):
 
-def findrow(i):
-	delrow= i.row()
-	print('del row=%s' % str(delrow) )
+	def __init__(self, parent=None):
+		super(ExecDatabaseDemo , self).__init__(parent)
+		
+		self.db = QSqlDatabase.addDatabase('QSQLITE')
+		self.db.setDatabaseName('./db/database.db')
+		# 打开数据库
+		self.db.open()
+		 
 
-if __name__ == '__main__':
-	app =  QApplication(sys.argv)
-	db = QSqlDatabase.addDatabase('QSQLITE')
-	db.setDatabaseName('./db/database.db')
-	model = QSqlTableModel()
-	delrow = -1
-	initializeModel(model)
-	view1 = createView("Table Model (View 1)", model) 
-	view1.clicked.connect(findrow)
-	
-	dlg = QDialog()
-	layout = QVBoxLayout()
-	layout.addWidget(view1)
-	addBtn = QPushButton("添加一行")
-	addBtn.clicked.connect(addrow)
-	layout.addWidget(addBtn)
+	def closeEvent(self, event):
+		# 关闭数据库
+		self.db.close()
     
-	delBtn = QPushButton("删除一行")
-	delBtn.clicked.connect(lambda: model.removeRow(view1.currentIndex().row()))
-	layout.addWidget(delBtn)
-	dlg.setLayout(layout)
-	dlg.setWindowTitle("Database 例子")
-	dlg.resize(430, 450)
-	dlg.show()
+if __name__ == '__main__':
+	app = QApplication(sys.argv)
+	demo = ExecDatabaseDemo()
+	demo.show()
 	sys.exit(app.exec_())
-
-	
-	
-	
+		
--- Chapter09/qt09_db03.py
+++ Chapter09/qt09_db03.py
@@ -11,65 +11,57 @@ import sys
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
-from PyQt5.QtSql import QSqlDatabase , QSqlQuery
+from PyQt5.QtSql import QSqlDatabase , QSqlTableModel
+from PyQt5.QtCore import Qt 
 
-def createDB():
-    # 添加数据库
-	db =  QSqlDatabase.addDatabase('QSQLITE')
-	# 设置数据库名称
-	db.setDatabaseName('./db/database.db')
-	# 判断是否打开
-	if not db.open():
-		QMessageBox.critical(None,  ("Cannot open database"),
-		("Unable to establish a database connection. \n"
-		"This example needs SQLite support. Please read "
-		"the Qt SQL driver documentation for information "
-		"how to build it.\n\n"
-		"Click Cancel to exit."),
-		QMessageBox.Cancel)
-		return False
-		
-	# 声明数据库查询对象
-	query = QSqlQuery()
-	# 创建表
-	query.exec("create table student(id int primary key, name vchar, sex vchar, age int, deparment vchar)")
+def initializeModel(model):
+	model.setTable('people')
+	model.setEditStrategy( QSqlTableModel.OnFieldChange)
+	model.select()
+	model.setHeaderData(0, Qt.Horizontal, "ID")
+	model.setHeaderData(1, Qt.Horizontal, "name")
+	model.setHeaderData(2, Qt.Horizontal, "address")
 	
-	#添加记录
-	query.exec("insert into student values(1,'张三1','男',20,'计算机')")
-	query.exec("insert into student values(2,'李四1','男',19,'经管')")
-	query.exec("insert into student values(3,'王五1','男',22,'机械')")
-	query.exec("insert into student values(4,'赵六1','男',21,'法律')")
-	query.exec("insert into student values(5,'小明1','男',20,'英语')")
-	query.exec("insert into student values(6,'小李1','女',19,'计算机')")
-	query.exec("insert into student values(7,'小张1','男',20,'机械')")
-	query.exec("insert into student values(8,'小刚1','男',19,'经管')")
-	query.exec("insert into student values(9,'张三2','男',21,'计算机')")
-	query.exec("insert into student values(10,'张三3','女',20,'法律')")
-	query.exec("insert into student values(11,'王五2','男',19,'经管')")
-	query.exec("insert into student values(12,'张三4','男',20,'计算机')")
-	query.exec("insert into student values(13,'小李2','男',20,'机械')")
-	query.exec("insert into student values(14,'李四2','女',19,'经管')")
-	query.exec("insert into student values(15,'赵六3','男',21,'英语')")
-	query.exec("insert into student values(16,'李四2','男',19,'法律')")
-	query.exec("insert into student values(17,'小张2','女',22,'经管')")
-	query.exec("insert into student values(18,'李四3','男',21,'英语')")
-	query.exec("insert into student values(19,'小李3','女',19,'法律')")
-	query.exec("insert into student values(20,'王五3','女',20,'机械')")
-	query.exec("insert into student values(21,'张三4','男',22,'计算机')")
-	query.exec("insert into student values(22,'小李2','男',20,'法律')")
-	query.exec("insert into student values(23,'张三5','男',19,'经管')")
-	query.exec("insert into student values(24,'小张3','女',20,'计算机')")
-	query.exec("insert into student values(25,'李四4','男',22,'英语')")
-	query.exec("insert into student values(26,'赵六2','男',20,'机械')")
-	query.exec("insert into student values(27,'小李3','女',19,'英语')")
-	query.exec("insert into student values(28,'王五4','男',21,'经管')")
-	# 关闭数据库
-	db.close()
+def createView(title, model):
+	view =  QTableView()
+	view.setModel(model)
+	view.setWindowTitle(title)
+	return view
+	
+def addrow():
+	ret = model.insertRows( model.rowCount(), 1 )
+	print( 'insertRows=%s' %str(ret) )
 
-	return True
+def findrow(i):
+	delrow= i.row()
+	print('del row=%s' % str(delrow) )
 
 if __name__ == '__main__':
 	app =  QApplication(sys.argv)
-	createDB() 
+	db = QSqlDatabase.addDatabase('QSQLITE')
+	db.setDatabaseName('./db/database.db')
+	model = QSqlTableModel()
+	delrow = -1
+	initializeModel(model)
+	view1 = createView("Table Model (View 1)", model) 
+	view1.clicked.connect(findrow)
+	
+	dlg = QDialog()
+	layout = QVBoxLayout()
+	layout.addWidget(view1)
+	addBtn = QPushButton("添加一行")
+	addBtn.clicked.connect(addrow)
+	layout.addWidget(addBtn)
+    
+	delBtn = QPushButton("删除一行")
+	delBtn.clicked.connect(lambda: model.removeRow(view1.currentIndex().row()))
+	layout.addWidget(delBtn)
+	dlg.setLayout(layout)
+	dlg.setWindowTitle("Database 例子")
+	dlg.resize(430, 450)
+	dlg.show()
 	sys.exit(app.exec_())
-		
+
+	
+	
+	
