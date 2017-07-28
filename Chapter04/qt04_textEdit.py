b79f6deea8e42683c571749515537daa269d5084
wangshuo <xpws2006@163.com>
2017-07-28 13:59:18 +0800

    QTextEdit例子

---
+++ Chapter04/qt04_textEdit.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+'''
+    【简介】
+	PyQt5中 QTextEdit例子       
+  
+'''
+
+from PyQt5.QtWidgets import QApplication,  QWidget ,  QTextEdit, QVBoxLayout , QPushButton
+import sys  
+
+class TextEditDemo(QWidget):
+	def __init__(self, parent=None):
+		super(TextEditDemo, self).__init__(parent)
+		self.setWindowTitle("QTextEdit 例子")
+		self.resize(300, 270)    
+		self.textEdt = QTextEdit( )      
+		self.btnPress1 = QPushButton("显示文本")
+		self.btnPress2 = QPushButton("显示HTML")        
+		layout = QVBoxLayout()
+		layout.addWidget(self.textEdt)
+		layout.addWidget(self.btnPress1)   
+		layout.addWidget(self.btnPress2)   		
+		self.setLayout(layout)
+		self.btnPress1.clicked.connect(self.btnPress1_Clicked)
+		self.btnPress2.clicked.connect(self.btnPress2_Clicked)
+		
+	def btnPress1_Clicked(self):
+		#print( self.textEdt.text())
+		self.textEdt.setPlainText("Hello PyQt5!\n点击按钮")
+
+	def btnPress2_Clicked(self):
+		#print( self.textEdt.text())
+		self.textEdt.setHtml("<font color='red' size='6'><red>Hello PyQt5!\n点击按钮。</font>")
+		
+if __name__ == "__main__":       
+	app = QApplication(sys.argv)
+	win = TextEditDemo()	
+	win.show()	
+	sys.exit(app.exec_())
