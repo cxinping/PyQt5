bc008da2260c6ac4e18eba25faf1313670416317
wangshuo <xpws2006@163.com>
2017-08-02 17:56:52 +0800

    完善测试代码

--- Chapter05/qt05_property.py
+++ Chapter05/qt05_property.py
@@ -1,22 +1,22 @@
 # -*- coding: utf-8 -*-
 
-from PyQt5.QtCore import QObject
-from PyQt5.QtCore import pyqtProperty
+from PyQt5.QtCore import QObject, pyqtProperty
 
 class MyObject(QObject):
-  def __init__(self, inVal=42):
-    self.val = inVal
-    
-  def readVal(self):
-       print('read val: %s' %  self.val )  
-       return self.val
+	def __init__(self, inVal=42):
+		self.val = inVal
 
-  def setVal(self,val):
-      print('set val: %s' %  val )    
-      self.val = val
-    
-  pp = pyqtProperty(int, readVal, setVal )
+	def readVal(self):
+		print('read val: %s' %  self.val )  
+		return self.val
 
-obj = MyObject()
-obj.pp = 10
-print( 'get val: %s' % obj.pp) 
+	def setVal(self,val):
+		print('set val: %s' %  val )    
+		self.val = val
+
+	pp = pyqtProperty(int, readVal, setVal )
+		
+if __name__ == '__main__':
+	obj = MyObject()
+	obj.pp = 10
+	print( 'get val: %s' % obj.pp) 
